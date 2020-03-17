81d9f1a8381c760de05d8dd37cf60d7252c5d1c2
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-03-17 16:46:07 +0100

    implemented initial version of obj importer, added demo scene with example models

---
+++ Assets/Examples/Importers/ObjImporter/ObjImporterDemo.cs
@@ -0,0 +1,29 @@
+﻿using i5.Toolkit.ModelImporters;
+using i5.Toolkit.ProceduralGeometry;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class ObjImporterDemo : MonoBehaviour
+{
+    [Multiline]
+    public string objData;
+
+    private MeshFilter meshFilter;
+
+    private void Awake()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            string[] lines = objData.Split('\n');
+            GeometryConstructor gc = ObjImporter.ParseObjText(lines);
+            meshFilter.sharedMesh = gc.ConstructMesh();
+        }
+    }
+}
--- Assets/i5 Toolkit/Importers/ObjImporter.cs
+++ Assets/i5 Toolkit/Importers/ObjImporter.cs
@@ -1,6 +1,7 @@
 ﻿using i5.Toolkit.ProceduralGeometry;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 
 namespace i5.Toolkit.ModelImporters
@@ -11,7 +12,7 @@ namespace i5.Toolkit.ModelImporters
         /// Parses the content of an obj file
         /// </summary>
         /// <param name="contentLines">The content of the obj file, sorted into lines</param>
-        private static void ParseObjText(string[] contentLines)
+        public static GeometryConstructor ParseObjText(string[] contentLines)
         {
             GeometryConstructor geometryConstructor = new GeometryConstructor();
 
@@ -22,14 +23,17 @@ namespace i5.Toolkit.ModelImporters
                 if (line.StartsWith("v "))
                 {
                     bool success = false;
-                    string[] strValues = line.Split(' ');
+                    // remove the v at the beginning and then split by spaces to get the coordinates
+                    string[] strValues = line.Substring(1).Trim().Split(' ');
                     // there should be three coordinates (but can also be four for the w coordinate)
                     if (strValues.Length >= 3)
                     {
                         float vCoord1, vCoord2, vCoord3;
                         // parse the first three coordinates
                         // TODO: handle the case that 4 coordinate are defined
-                        if (float.TryParse(strValues[0], out vCoord1) && float.TryParse(strValues[1], out vCoord2) && float.TryParse(strValues[2], out vCoord3))
+                        if (float.TryParse(strValues[0], NumberStyles.Any, CultureInfo.InvariantCulture, out vCoord1)
+                            && float.TryParse(strValues[1], NumberStyles.Any, CultureInfo.InvariantCulture, out vCoord2)
+                            && float.TryParse(strValues[2], NumberStyles.Any, CultureInfo.InvariantCulture, out vCoord3))
                         {
                             geometryConstructor.AddVertex(new Vector3(vCoord1, vCoord2, vCoord3));
                             success = true;
@@ -54,7 +58,8 @@ namespace i5.Toolkit.ModelImporters
                 // face definitions are defined with an "f" at the beginning
                 else if (line.StartsWith("f "))
                 {
-                    string[] vertexData = line.Split(' ');
+                    // remove the f and split by spaces to get the individual indices of the face
+                    string[] vertexData = line.Substring(1).Trim().Split(' ');
                     if (vertexData.Length != 3)
                     {
                         Debug.LogError("[ObjImporter] ObjImporter only supports triangular faces. If you use polygons, triangulate the mesh before importing");
@@ -65,17 +70,56 @@ namespace i5.Toolkit.ModelImporters
                     int[] vertexUVIndices = new int[vertexData.Length];
                     int[] vertexNormalIndices = new int[vertexData.Length];
 
+                    // go over all vertex data
                     for (int i = 0; i < vertexData.Length; i++)
                     {
+                        bool parseSuccess = true;
                         string[] strIndices = vertexData[i].Split(new char[] { '/' }, System.StringSplitOptions.None);
-                        // if no "/" => we have a single number which is the vertex index
-                        if (strIndices.Length == 1)
-                        {
 
+                        // in any case: parse the first index
+                        // tmpParsedIndex: temporary variable which is used to get the index which was just read
+                        // tmpParsedIndex is reused for all three indices
+                        int tmpParsedIndex;
+                        parseSuccess &= int.TryParse(strIndices[0], out tmpParsedIndex);
+                        // correct by one since obj format starts counting at 1 and not 0
+                        vertexIndices[i] = tmpParsedIndex - 1;
+
+                        // also get the other two fields if they exist
+                        if (parseSuccess && strIndices.Length == 3)
+                        {
+                            // the middle entry might be empty
+                            if (!string.IsNullOrEmpty(strIndices[1]))
+                            {
+                                parseSuccess &= int.TryParse(strIndices[1], out tmpParsedIndex);
+                                // correct by one since obj format starts counting at 1 and not 0
+                                vertexUVIndices[i] = tmpParsedIndex - 1;
+                            }
+                            // parse the last entry
+                            parseSuccess &= int.TryParse(strIndices[2], out tmpParsedIndex);
+                            vertexNormalIndices[i] = tmpParsedIndex - 1;
+                        }
+                        // if we do not have a single number and not the format 1/2/3 or 1//2, the format is wrong
+                        else if (strIndices.Length != 1)
+                        {
+                            parseSuccess = false;
+                        }
+
+                        if (!parseSuccess)
+                        {
+                            Debug.LogError("[ObjImporter] Unable to parse indices: " + vertexData[i]);
                         }
                     }
+
+                    geometryConstructor.AddTriangle(vertexIndices[0], vertexIndices[1], vertexIndices[2]);
+                    // TODO: add UV and normals to geometryConstructor
+                }
+                else if (line.StartsWith("#"))
+                {
+                    Debug.Log("[ObjImporter] Found a comment: " + line.Substring(1));
                 }
             }
+
+            return geometryConstructor;
         }
     }
 }

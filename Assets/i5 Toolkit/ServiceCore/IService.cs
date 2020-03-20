2ea7f286bea2a837b85b0b3e2ece402903680e69
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-03-20 16:48:44 +0100

    added service core classes

---
+++ Assets/i5 Toolkit/ServiceCore/IService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace i5.Toolkit.ServiceCore
+{
+    public interface IService
+    {
+        bool Enabled { get; set; }
+
+        void Initialize();
+        void Cleanup();
+    }
+}
---
+++ Assets/i5 Toolkit/ServiceCore/ServiceManager.cs
@@ -0,0 +1,59 @@
+﻿using i5.Toolkit.Utilities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace i5.Toolkit.ServiceCore
+{
+    public class ServiceManager : MonoBehaviour
+    {
+        private static Dictionary<object, IService> registeredServices = new Dictionary<object, IService>();
+
+        private static List<IUpdateableService> updateableServices = new List<IUpdateableService>();
+
+        private static ServiceManager instance;
+
+        public static ServiceManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    GameObject serviceManagerObj = ObjectPool<GameObject>.RequestResource(() => { return new GameObject(); });
+                    serviceManagerObj.name = "Service Manager";
+                    instance = serviceManagerObj.AddComponent<ServiceManager>();
+                }
+                return instance;
+            }
+        }
+
+        public static void RegisterService<T>(T service) where T : IService
+        {
+            registeredServices.Add(typeof(T), service);
+
+            if (service.GetType() == typeof(IUpdateableService))
+            {
+                updateableServices.Add((IUpdateableService)service);
+            }
+        }
+
+        public static void RemoveService<T>(T service) where T : IService
+        {
+            registeredServices.Remove(service);
+            if (service.GetType() == typeof(IUpdateableService))
+            {
+                updateableServices.Remove((IUpdateableService)service);
+            }
+        }
+
+        public static T GetService<T>(T service) where T : IService
+        {
+            if (!registeredServices.ContainsKey(typeof(T)))
+            {
+                throw new InvalidOperationException("Tried to get unregistered service");
+            }
+            return (T)registeredServices[typeof(T)];
+        }
+    }
+}

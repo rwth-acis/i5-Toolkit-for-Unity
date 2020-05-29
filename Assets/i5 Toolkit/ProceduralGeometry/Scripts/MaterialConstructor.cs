bcd991c0c072bec225d6d56721df44bb6ea9c029
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-05-29 19:03:00 +0200

    added object constructor

---
+++ Assets/i5 Toolkit/ProceduralGeometry/Scripts/MaterialConstructor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialConstructor
+{
+    public Material ConstructMaterial()
+    {
+        return new Material(Shader.Find("Standard"));
+    }
+}
---
+++ Assets/i5 Toolkit/ProceduralGeometry/Scripts/ObjectConstructor.cs
@@ -0,0 +1,44 @@
+﻿using i5.Toolkit.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace i5.Toolkit.ProceduralGeometry
+{
+    public class ObjectConstructor
+    {
+        public GeometryConstructor GeometryConstructor { get; set; }
+        
+        public MaterialConstructor Material { get; set; }
+
+        public ObjectConstructor()
+        {
+            GeometryConstructor = new GeometryConstructor();
+            Material = new MaterialConstructor();
+        }
+
+        public ObjectConstructor(GeometryConstructor geometryConstructor)
+        {
+            GeometryConstructor = geometryConstructor;
+        }
+
+        public ObjectConstructor(GeometryConstructor geometryConstructor, MaterialConstructor material) : this(geometryConstructor)
+        {
+            Material = material;
+        }
+
+        public void ConstructObject()
+        {
+            GameObject go = ObjectPool<GameObject>.RequestResource(() => { return new GameObject("Object Constructor Result"); });
+
+            go.name = GeometryConstructor.Name;
+
+            MeshFilter meshFilter = ComponentUtilities.GetOrAddComponent<MeshFilter>(go);
+            MeshRenderer meshRenderer = ComponentUtilities.GetOrAddComponent<MeshRenderer>(go);
+
+            meshRenderer.material = Material.ConstructMaterial();
+            Mesh mesh = GeometryConstructor.ConstructMesh();
+            meshFilter.sharedMesh = mesh;
+        }
+    }
+}

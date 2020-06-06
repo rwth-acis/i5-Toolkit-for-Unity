00b4660614aa19e8dd7b28615d1be2b0d7fb3be9
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-06-06 22:51:41 +0200

    created mtl library service test file

---
+++ Assets/i5 Toolkit/Tests/EditModeTests/Importers/MtlLibraryServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using i5.Toolkit.ModelImporters;
+using i5.Toolkit.ServiceCore;
+using NUnit.Framework;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace i5.Toolkit.Tests.ModelImporters 
+{
+    public class MtlLibraryServiceTests
+    {
+        [SetUp]
+        public void ResetScene()
+        {
+            EditorSceneManager.OpenScene("Assets/i5 Toolkit/Tests/TestResources/SetupTestScene.unity");
+            ServiceManager.RegisterService(new MtlLibraryService());
+        }
+    }
+}

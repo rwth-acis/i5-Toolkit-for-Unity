5a963ab3244561bfaba29a05abac0642afc88785
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-06-10 13:21:31 +0200

    updated async tests of mtl library service

--- Assets/i5 Toolkit/Tests/EditModeTests/Importers/MtlLibraryServiceTests.cs
+++ Assets/i5 Toolkit/Tests/EditModeTests/Importers/MtlLibraryServiceTests.cs
@@ -42,36 +42,45 @@ namespace i5.Toolkit.Tests.ModelImporters
             mtlLibraryService = ServiceManager.GetService<MtlLibraryService>();
         }
 
-        [Test]
-        public async void LoadLibraryAsync_NewLibrary_LibraryLoadedReturnsTrue()
+        [UnityTest]
+        public IEnumerator LoadLibraryAsync_NewLibrary_LibraryLoadedReturnsTrue()
         {
             mtlLibraryService.ContentLoader = fakeContentLoader;
-            await LoadLibrary();
+            Task task = LoadLibrary();
+
+            yield return AsyncTest.WaitForTask(task);
 
             bool loaded = mtlLibraryService.LibraryLoaded(libraryName);
             Assert.IsTrue(loaded, "The library should have been loaded but is not displayed as loaded");
         }
 
-        [Test]
-        public async void LoadLibraryAsync_LoadFailed_LibraryLoadedReturnsFalse()
+        [UnityTest]
+        public IEnumerator LoadLibraryAsync_LoadFailed_LibraryLoadedReturnsFalse()
         {
             mtlLibraryService.ContentLoader = fakeContentLoader;
-            await LoadLibrary();
+            Task task = LoadLibrary();
+
+            yield return AsyncTest.WaitForTask(task);
 
             bool loaded = mtlLibraryService.LibraryLoaded(libraryName);
             Assert.IsFalse(loaded, "The import should have aborted but apparently, the library is shown as imported");
         }
 
-        [Test]
-        public async void LoadLibraryAsync_LoadLibraryMultipleTimes_NoErrorsAndLibraryLoadedTrue()
+        [UnityTest]
+        public IEnumerator LoadLibraryAsync_LoadLibraryMultipleTimes_NoErrorsAndLibraryLoadedTrue()
         {
             mtlLibraryService.ContentLoader = fakeContentLoader;
-            await LoadLibrary();
+            Task task = LoadLibrary();
+
+            yield return AsyncTest.WaitForTask(task);
 
             bool loaded = mtlLibraryService.LibraryLoaded(libraryName);
             Assert.IsTrue(loaded, "The library should have been loaded but is not displayed as loaded");
 
-            await LoadLibrary();
+            Task task2 = LoadLibrary();
+
+            yield return AsyncTest.WaitForTask(task2);
+
             LogAssert.Expect(LogType.Warning, new Regex(@"\w*was already loaded\w*"));
             loaded = mtlLibraryService.LibraryLoaded(libraryName);
             Assert.IsTrue(loaded, "Loading the same library multiple times makes the library show up as unloaded");
@@ -85,50 +94,78 @@ namespace i5.Toolkit.Tests.ModelImporters
             Assert.IsNull(res);
         }
 
-        [Test]
-        public async void GetMaterialConstructor_ExistentMatLibNonExistentMat_ReturnsNull()
+        [UnityTest]
+        public IEnumerator GetMaterialConstructor_ExistentMatLibNonExistentMat_ReturnsNull()
         {
             mtlLibraryService.ContentLoader = fakeContentLoader;
-            await LoadLibrary();
+            Task task = LoadLibrary();
+
+            yield return AsyncTest.WaitForTask(task);
+
             MaterialConstructor res = mtlLibraryService.GetMaterialConstructor(libraryName, "");
             Assert.IsNull(res);
         }
 
-        [Test]
-        public async void GetMaterialConstructor_ExistentMatLibExistentMat_ReturnsNotNull()
+        [UnityTest]
+        public IEnumerator GetMaterialConstructor_ExistentMatLibExistentMat_ReturnsNotNull()
         {
             mtlLibraryService.ContentLoader = fakeContentLoader;
-            await LoadLibrary();
+            Task task = LoadLibrary();
+
+            yield return AsyncTest.WaitForTask(task);
+
             MaterialConstructor res = mtlLibraryService.GetMaterialConstructor(libraryName, "BlueMat");
             Assert.IsNotNull(res);
         }
 
-        [Test]
-        public async void GetMaterialConstructor_ExistentMatLibExistentMat_MatConstrColorSet()
+        [UnityTest]
+        public IEnumerator GetMaterialConstructor_ExistentMatLibExistentMat_MatConstrColorSet()
         {
             mtlLibraryService.ContentLoader = fakeContentLoader;
-            await LoadLibrary();
+            Task task = LoadLibrary();
+
+            yield return AsyncTest.WaitForTask(task);
+
             MaterialConstructor res = mtlLibraryService.GetMaterialConstructor(libraryName, "BlueMat");
             Assert.IsNotNull(res);
             Assert.AreEqual(new Color(0.185991f, 0.249956f, 0.800000f), res.Color);
         }
 
-        [Test]
-        public async void GetMaterialConstructor_ExistentMatLibExistentMat_MatConstrNameSet()
+        [UnityTest]
+        public IEnumerator GetMaterialConstructor_ExistentMatLibExistentMat_MatConstrNameSet()
         {
             mtlLibraryService.ContentLoader = fakeContentLoader;
-            await LoadLibrary();
+            Task task = LoadLibrary();
+
+            yield return AsyncTest.WaitForTask(task);
+
             MaterialConstructor res = mtlLibraryService.GetMaterialConstructor(libraryName, "BlueMat");
             Assert.IsNotNull(res);
             Assert.AreEqual("BlueMat", res.Name);
         }
 
-        [Test]
-        public async void ExtendedLogging_Enabled_CommentsLogged()
+        [UnityTest]
+        public IEnumerator ExtendedLogging_Enabled_CommentsLogged()
         {
             mtlLibraryService.ContentLoader = fakeContentLoader;
-            await LoadLibrary();
+            mtlLibraryService.ExtendedLogging = true;
+            Task task = LoadLibrary();
+
+            yield return AsyncTest.WaitForTask(task);
+
             LogAssert.Expect(LogType.Log, new Regex(@"\w*Comment found\w*"));
+        }
+
+        [UnityTest]
+        public IEnumerator ExtendedLogging_Disabled_CommentsNotLogged()
+        {
+            mtlLibraryService.ContentLoader = fakeContentLoader;
+            mtlLibraryService.ExtendedLogging = false;
+            Task task = LoadLibrary();
+
+            yield return AsyncTest.WaitForTask(task);
+
+            LogAssert.NoUnexpectedReceived();
         }
 
         private async Task LoadLibrary()

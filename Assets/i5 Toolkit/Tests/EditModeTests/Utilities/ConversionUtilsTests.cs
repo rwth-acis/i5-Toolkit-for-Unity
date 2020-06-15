4c88c57929315abea64b2155237d88b8aad869be
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-06-15 19:12:35 +0200

    added unit tests for utility classes

---
+++ Assets/i5 Toolkit/Tests/EditModeTests/Utilities/ConversionUtilsTests.cs
@@ -0,0 +1,262 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using i5.Toolkit.Utilities;
+using NUnit.Framework;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace i5.Toolkit.Tests.Utilities
+{
+    /// <summary>
+    /// Tests for the conversion utils class
+    /// </summary>
+    public class ConversionUtilsTests
+    {
+        /// <summary>
+        /// Resets the scene before each test
+        /// </summary>
+        [SetUp]
+        public void ResetScene()
+        {
+            EditorSceneManager.OpenScene("Assets/i5 Toolkit/Tests/TestResources/SetupTestScene.unity");
+        }
+
+        /// <summary>
+        /// Checks if the zero vector is correctly converted
+        /// </summary>
+        [Test]
+        public void Vector3ToColor_Zero_ColorComponentsMatch()
+        {
+            Vector3 v = Vector3.zero;
+            Color c = v.ToColor();
+
+            Assert.AreEqual(v.x, c.r);
+            Assert.AreEqual(v.y, c.g);
+            Assert.AreEqual(v.z, c.b);
+            Assert.AreEqual(1, c.a);
+        }
+
+        /// <summary>
+        /// Checks if components smaller or equal to one are converted correctly
+        /// </summary>
+        [Test]
+        public void Vector3ToColor_Components1_ColorComponentsMatch()
+        {
+            Vector3 v = new Vector3(1, 1, 1);
+            Color c = v.ToColor();
+
+            Assert.AreEqual(v.x, c.r);
+            Assert.AreEqual(v.y, c.g);
+            Assert.AreEqual(v.z, c.b);
+            Assert.AreEqual(1, c.a);
+        }
+
+        /// <summary>
+        /// Checks if components bigger than one are converted correctly
+        /// </summary>
+        [Test]
+        public void Vector3ToColor_ComponentsBigger1_ColorComponentsMatch()
+        {
+            Vector3 v = new Vector3(10, 100, 1000);
+            Color c = v.ToColor();
+
+            Assert.AreEqual(v.x, c.r);
+            Assert.AreEqual(v.y, c.g);
+            Assert.AreEqual(v.z, c.b);
+            Assert.AreEqual(1, c.a);
+        }
+
+        /// <summary>
+        /// Checks if negative components are converted correctly
+        /// </summary>
+        [Test]
+        public void Vector3ToColor_ComponentsNegative_ColorComponentsMatch()
+        {
+            Vector3 v = new Vector3(-1, -1, -1);
+            Color c = v.ToColor();
+
+            Assert.AreEqual(v.x, c.r);
+            Assert.AreEqual(v.y, c.g);
+            Assert.AreEqual(v.z, c.b);
+            Assert.AreEqual(1, c.a);
+        }
+
+
+        /// <summary>
+        /// Checks if the zero vector is correctly converted
+        /// </summary>
+        [Test]
+        public void Vector4ToColor_Zero_ColorComponentsMatch()
+        {
+            Vector4 v = Vector4.zero;
+            Color c = v.ToColor();
+
+            Assert.AreEqual(v.x, c.r);
+            Assert.AreEqual(v.y, c.g);
+            Assert.AreEqual(v.z, c.b);
+            Assert.AreEqual(v.w, c.a);
+        }
+
+        /// <summary>
+        /// Checks if components smaller or equal to one are converted correctly
+        /// </summary>
+        [Test]
+        public void Vector4ToColor_Components1_ColorComponentsMatch()
+        {
+            Vector4 v = new Vector4(1, 1, 1, 1);
+            Color c = v.ToColor();
+
+            Assert.AreEqual(v.x, c.r);
+            Assert.AreEqual(v.y, c.g);
+            Assert.AreEqual(v.z, c.b);
+            Assert.AreEqual(v.w, c.a);
+        }
+
+        /// <summary>
+        /// Checks if components bigger than one are converted correctly
+        /// </summary>
+        [Test]
+        public void Vector4ToColor_ComponentsBigger1_ColorComponentsMatch()
+        {
+            Vector4 v = new Vector4(10, 100, 1000, 10000);
+            Color c = v.ToColor();
+
+            Assert.AreEqual(v.x, c.r);
+            Assert.AreEqual(v.y, c.g);
+            Assert.AreEqual(v.z, c.b);
+            Assert.AreEqual(v.w, c.a);
+        }
+
+        /// <summary>
+        /// Checks if negative components are converted correctly
+        /// </summary>
+        [Test]
+        public void Vector4ToColor_ComponentsNegative_ColorComponentsMatch()
+        {
+            Vector4 v = new Vector4(-1, -1, -1, -1);
+            Color c = v.ToColor();
+
+            Assert.AreEqual(v.x, c.r);
+            Assert.AreEqual(v.y, c.g);
+            Assert.AreEqual(v.z, c.b);
+            Assert.AreEqual(v.w, c.a);
+        }
+
+        /// <summary>
+        /// Checks if black is correctly converted
+        /// </summary>
+        [Test]
+        public void ColorToVector3_Zero_VectorComponentsMatch()
+        {
+            Color c = new Color(0, 0, 0, 0);
+            Vector3 v = c.ToVector3();
+
+            Assert.AreEqual(c.r, v.x);
+            Assert.AreEqual(c.g, v.y);
+            Assert.AreEqual(c.b, v.z);
+        }
+
+        /// <summary>
+        /// Checks if components smaller or equal to one are converted correctly
+        /// </summary>
+        [Test]
+        public void ColorToVector3_Components1_VectorComponentsMatch()
+        {
+            Color c = new Color(1, 1, 1, 1);
+            Vector3 v = c.ToVector3();
+
+            Assert.AreEqual(c.r, v.x);
+            Assert.AreEqual(c.g, v.y);
+            Assert.AreEqual(c.b, v.z);
+        }
+
+        /// <summary>
+        /// Checks if components bigger than one are converted correctly
+        /// </summary>
+        [Test]
+        public void ColorToVector3_ComponentsBigger1_VectorComponentsMatch()
+        {
+            Color c = new Color(10, 100, 1000, 10000);
+            Vector3 v = c.ToVector3();
+
+            Assert.AreEqual(c.r, v.x);
+            Assert.AreEqual(c.g, v.y);
+            Assert.AreEqual(c.b, v.z);
+        }
+
+        /// <summary>
+        /// Checks if negative components are converted correctly
+        /// </summary>
+        [Test]
+        public void ColorToVector3_ComponentsNegative_VectorComponentsMatch()
+        {
+            Color c = new Color(-1, -1, -1, -1);
+            Vector3 v = c.ToVector3();
+
+            Assert.AreEqual(c.r, v.x);
+            Assert.AreEqual(c.g, v.y);
+            Assert.AreEqual(c.b, v.z);
+        }
+
+        /// <summary>
+        /// Checks if black is correctly converted
+        /// </summary>
+        [Test]
+        public void ColorToVector4_Zero_VectorComponentsMatch()
+        {
+            Color c = new Color(0, 0, 0, 0);
+            Vector4 v = c.ToVector4();
+
+            Assert.AreEqual(c.r, v.x);
+            Assert.AreEqual(c.g, v.y);
+            Assert.AreEqual(c.b, v.z);
+            Assert.AreEqual(c.a, v.w);
+        }
+
+        /// <summary>
+        /// Checks if components smaller or equal to one are converted correctly
+        /// </summary>
+        [Test]
+        public void ColorToVector4_Components1_VectorComponentsMatch()
+        {
+            Color c = new Color(1, 1, 1, 1);
+            Vector4 v = c.ToVector4();
+
+            Assert.AreEqual(c.r, v.x);
+            Assert.AreEqual(c.g, v.y);
+            Assert.AreEqual(c.b, v.z);
+            Assert.AreEqual(c.a, v.w);
+        }
+
+        /// <summary>
+        /// Checks if components bigger than one are converted correctly
+        /// </summary>
+        [Test]
+        public void ColorToVector4_ComponentsBigger1_VectorComponentsMatch()
+        {
+            Color c = new Color(10, 100, 1000, 10000);
+            Vector4 v = c.ToVector4();
+
+            Assert.AreEqual(c.r, v.x);
+            Assert.AreEqual(c.g, v.y);
+            Assert.AreEqual(c.b, v.z);
+            Assert.AreEqual(c.a, v.w);
+        }
+
+        /// <summary>
+        /// Checks if negative components are converted correctly
+        /// </summary>
+        [Test]
+        public void ColorToVector4_ComponentsNegative_VectorComponentsMatch()
+        {
+            Color c = new Color(-1, -1, -1, -1);
+            Vector4 v = c.ToVector4();
+
+            Assert.AreEqual(c.r, v.x);
+            Assert.AreEqual(c.g, v.y);
+            Assert.AreEqual(c.b, v.z);
+            Assert.AreEqual(c.a, v.w);
+        }
+    }
+}
---
+++ Assets/i5 Toolkit/Tests/EditModeTests/Utilities/ParserUtilsTests.cs
@@ -0,0 +1,369 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using i5.Toolkit.Utilities;
+using NUnit.Framework;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace i5.Toolkit.Tests.Utilities
+{
+    /// <summary>
+    /// Tests for the parser utils tests
+    /// </summary>
+    public class ParserUtilsTests
+    {
+        /// <summary>
+        /// Resets the scene before each test
+        /// </summary>
+        [SetUp]
+        public void ResetScene()
+        {
+            EditorSceneManager.OpenScene("Assets/i5 Toolkit/Tests/TestResources/SetupTestScene.unity");
+        }
+
+        #region TryParseSpaceSeparatedVector2
+
+        /// <summary>
+        /// Checks that a vector in the correct format is parsed
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector2_CorrectFormat_ReturnsTrue()
+        {
+            string vString = "1 1";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector2(vString, out Vector2 vector);
+
+            Assert.IsTrue(res);
+        }
+
+        /// <summary>
+        /// Checks that a vector in the correct format gives the correct value
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector2_CorrectFormat_OutParamCorrect()
+        {
+            string vString = "1 1";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector2(vString, out Vector2 vector);
+
+            Assert.AreEqual(new Vector2(1,1), vector);
+        }
+
+        /// <summary>
+        /// Checks that invalid strings in the right format return false
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector2_InvalidNumber_ReturnsFalse()
+        {
+            string vString = "1 s";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector2(vString, out Vector2 vector);
+
+            Assert.IsFalse(res);
+        }
+
+        /// <summary>
+        /// Checks that invalid strings in the right format give a default value
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector2_InvalidNumber_OutParamDefault()
+        {
+            string vString = "1 s";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector2(vString, out Vector2 vector);
+
+            Assert.AreEqual(default(Vector2), vector);
+        }
+
+        /// <summary>
+        /// Checks that strings with just one number return false
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector2_OneNumber_ReturnsFalse()
+        {
+            string vString = "1";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector2(vString, out Vector2 vector);
+
+            Assert.IsFalse(res);
+        }
+
+        /// <summary>
+        /// Checks that strings with just one number give a default value
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector2_OneNumber_OutParamDefault()
+        {
+            string vString = "1";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector2(vString, out Vector2 vector);
+
+            Assert.AreEqual(default(Vector2), vector);
+        }
+
+        /// <summary>
+        /// Checks that a string with three numbers returns true
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector2_ThreeNumbers_ReturnsTrue()
+        {
+            string vString = "1 1 1";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector2(vString, out Vector2 vector);
+
+            Assert.IsTrue(res);
+        }
+
+        /// <summary>
+        /// Checks that a string with three numbers give a default value
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector2_ThreeNumbers_OutParamCorrect()
+        {
+            string vString = "1 1 1";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector2(vString, out Vector2 vector);
+
+            Assert.AreEqual(new Vector2(1,1), vector);
+        }
+
+        /// <summary>
+        /// Checks that a string with four numbers returns false
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector2_FourNumbers_ReturnsFalse()
+        {
+            string vString = "1 1 1 1";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector2(vString, out Vector2 vector);
+
+            Assert.IsFalse(res);
+        }
+
+        /// <summary>
+        /// Checks that a string with four numbers gives a default value
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector2_FourNumbers_OutParamDefault()
+        {
+            string vString = "1 1 1 1";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector2(vString, out Vector2 vector);
+
+            Assert.AreEqual(default(Vector2), vector);
+        }
+
+        /// <summary>
+        /// Checks that a string in the invalid format returns false
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector2_InvalidFormat_ReturnsFalse()
+        {
+            string vString = "hello";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector2(vString, out Vector2 vector);
+
+            Assert.IsFalse(res);
+        }
+
+        /// <summary>
+        /// Checks that a string in the invalid format give a default value
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector2_InvalidFormat_OutParamDefault()
+        {
+            string vString = "hello";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector2(vString, out Vector2 vector);
+
+            Assert.AreEqual(default(Vector2), vector);
+        }
+
+        /// <summary>
+        /// Checks that a string where numbers are separated by multiple spaces return true
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector2_VectorWithTwoSpaces_ReturnsTrue()
+        {
+            string vString = "1  1";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector2(vString, out Vector2 vector);
+
+            Assert.IsTrue(res);
+        }
+
+        /// <summary>
+        /// Checks that a string where numbers are separated by multiple spaces gives the correct value
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector2_VectorWithTwoSpaces_OutParamCorrect()
+        {
+            string vString = "1  1";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector2(vString, out Vector2 vector);
+
+            Assert.AreEqual(new Vector2(1,1), vector);
+        }
+
+        #endregion
+
+        #region TryParseSpaceSeparatedVector3
+
+        /// <summary>
+        /// Checks that a vector in the correct format is parsed
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector3_CorrectFormat_ReturnsTrue()
+        {
+            string vString = "1 1 1";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector3(vString, out Vector3 vector);
+
+            Assert.IsTrue(res);
+        }
+
+        /// <summary>
+        /// Checks that a vector in the correct format gives the correct value
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector3_CorrectFormat_OutParamCorrect()
+        {
+            string vString = "1 1 1";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector3(vString, out Vector3 vector);
+
+            Assert.AreEqual(new Vector3(1, 1, 1), vector);
+        }
+
+        /// <summary>
+        /// Checks that invalid strings in the right format return false
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector3_InvalidNumber_ReturnsFalse()
+        {
+            string vString = "1 s 1";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector3(vString, out Vector3 vector);
+
+            Assert.IsFalse(res);
+        }
+
+        /// <summary>
+        /// Checks that invalid strings in the right format give a default value
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector3_InvalidNumber_OutParamDefault()
+        {
+            string vString = "1 s 1";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector3(vString, out Vector3 vector);
+
+            Assert.AreEqual(default(Vector3), vector);
+        }
+
+        /// <summary>
+        /// Checks that strings with just one number return false
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector3_OneNumber_ReturnsFalse()
+        {
+            string vString = "1";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector3(vString, out Vector3 vector);
+
+            Assert.IsFalse(res);
+        }
+
+        /// <summary>
+        /// Checks that strings with just one number give a default value
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector3_OneNumber_OutParamDefault()
+        {
+            string vString = "1";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector3(vString, out Vector3 vector);
+
+            Assert.AreEqual(default(Vector3), vector);
+        }
+
+        /// <summary>
+        /// Checks that a string with three numbers returns false
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector3_TwoNumbers_ReturnsFalse()
+        {
+            string vString = "1 1";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector3(vString, out Vector3 vector);
+
+            Assert.IsFalse(res);
+        }
+
+        /// <summary>
+        /// Checks that a string with two numbers gives a default value
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector3_TwoNumbers_OutParamCorrect()
+        {
+            string vString = "1 1";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector3(vString, out Vector3 vector);
+
+            Assert.AreEqual(default(Vector3), vector);
+        }
+
+        /// <summary>
+        /// Checks that a string with four numbers returns true
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector3_FourNumbers_ReturnsTrue()
+        {
+            string vString = "1 1 1 1";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector3(vString, out Vector3 vector);
+
+            Assert.IsTrue(res);
+        }
+
+        /// <summary>
+        /// Checks that a string with four numbers gives the correct value
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector3_FourNumbers_OutParamCorrect()
+        {
+            string vString = "1 1 1 1";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector3(vString, out Vector3 vector);
+
+            Assert.AreEqual(new Vector3(1,1,1), vector);
+        }
+
+        /// <summary>
+        /// Checks that a string in the invalid format returns false
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector3_InvalidFormat_ReturnsFalse()
+        {
+            string vString = "hello";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector3(vString, out Vector3 vector);
+
+            Assert.IsFalse(res);
+        }
+
+        /// <summary>
+        /// Checks that a string in the invalid format give a default value
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector3_InvalidFormat_OutParamDefault()
+        {
+            string vString = "hello";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector3(vString, out Vector3 vector);
+
+            Assert.AreEqual(default(Vector3), vector);
+        }
+
+        /// <summary>
+        /// Checks that a string where numbers are separated by multiple spaces return true
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector3_VectorWithTwoSpaces_ReturnsTrue()
+        {
+            string vString = "1  1 1";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector3(vString, out Vector3 vector);
+
+            Assert.IsTrue(res);
+        }
+
+        /// <summary>
+        /// Checks that a string where numbers are separated by multiple spaces gives the correct value
+        /// </summary>
+        [Test]
+        public void TryParseSpaceSeparatedVector3_VectorWithTwoSpaces_OutParamCorrect()
+        {
+            string vString = "1  1 1";
+            bool res = ParserUtils.TryParseSpaceSeparatedVector3(vString, out Vector3 vector);
+
+            Assert.AreEqual(new Vector3(1, 1, 1), vector);
+        }
+
+        #endregion
+    }
+}

45a35a58980fec00f7d7e4a74ee53776d9cce988
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-06-20 21:12:05 +0200

    created documentation object

---
+++ Assets/i5 Toolkit/Documentation Objects/Scripts/DocumentationObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace i5.Toolkit.SceneDocumentation
+{
+    public class DocumentationObject : MonoBehaviour
+    {
+        public string title;
+        public string description;
+        public string url;
+    }
+}

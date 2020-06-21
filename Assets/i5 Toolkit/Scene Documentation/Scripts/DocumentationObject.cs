5753ae707fb62fa01fc4f71398a75d1087005a0b
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-06-21 19:47:22 +0200

    package resource copying, added documentation

--- Assets/i5 Toolkit/Scene Documentation/Scripts/DocumentationObject.cs
+++ Assets/i5 Toolkit/Scene Documentation/Scripts/DocumentationObject.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 
 namespace i5.Toolkit.SceneDocumentation
 {
+    /// <summary>
+    /// A special component which provides documentation information in the editor
+    /// </summary>
     public class DocumentationObject : MonoBehaviour
     {
+        /// <summary>
+        /// The title of the documentation
+        /// </summary>
         public string title;
+        /// <summary>
+        /// A description of the documented highlight
+        /// </summary>
         [TextArea]
         public string description;
+        /// <summary>
+        /// The type of documentation (e.g. information, todo mark or bug mark)
+        /// Based on this, the icon is changed
+        /// </summary>
         public DocumentationType type;
+        /// <summary>
+        /// A url to further documentation on the matter
+        /// </summary>
         public string url;
 
+        /// <summary>
+        /// Called by the Unity Editor to draw gizmos
+        /// Shows the corresponding icon at the position of the Transform
+        /// </summary>
         private void OnDrawGizmos()
         {
             switch(type)
@@ -29,8 +49,11 @@ namespace i5.Toolkit.SceneDocumentation
         }
     }
 
+    /// <summary>
+    /// The different types of Documentation which are available
+    /// </summary>
     public enum DocumentationType
     {
-        INFO, TODO, BUG
+        INFO, TODO, BUG, NO_ICON
     }
 }

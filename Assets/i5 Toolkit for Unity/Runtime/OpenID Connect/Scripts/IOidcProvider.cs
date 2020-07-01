3242bad044af0b6ad8faf0e0d13d992fa1641d86
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-07-01 19:17:20 +0200

    added core structure for OpenID connect client

---
+++ Assets/i5 Toolkit for Unity/Runtime/OpenID Connect/Scripts/IOidcProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace i5.Toolkit.Core.OpenIDConnectClient
+{
+    public interface IOidcProvider
+    {
+        void OpenLoginPage(string clientId, string[] scopes, string redirectUri);
+
+        string RetrieveAccessToken(Dictionary<string, string> arguments);
+
+        bool IsAccessTokenValid(string accessToken);
+
+        IUserInfo GetUserInfo(string accessToken);
+    }
+}
---
+++ Assets/i5 Toolkit for Unity/Runtime/OpenID Connect/Scripts/IUserInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace i5.Toolkit.Core.OpenIDConnectClient
+{
+    public interface IUserInfo
+    {
+        string Username { get; }
+
+        string Email { get; }
+    }
+}

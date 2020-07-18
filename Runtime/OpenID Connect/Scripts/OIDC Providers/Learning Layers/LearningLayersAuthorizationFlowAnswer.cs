5668b16de825ec31e0b7038b0c6c3cfe18c684e6
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-07-18 22:08:27 +0200

    added access token retrieval

---
+++ Runtime/OpenID Connect/Scripts/OIDC Providers/Learning Layers/LearningLayersAuthorizationFlowAnswer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace i5.Toolkit.Core.OpenIDConnectClient
+{
+    [Serializable]
+    public class LearningLayersAuthorizationFlowAnswer
+    {
+        public string access_token;
+        public string token_type;
+        public int expires_in;
+        public string scope;
+        public string id_token;
+        public string error;
+        public string error_description;
+    }
+}

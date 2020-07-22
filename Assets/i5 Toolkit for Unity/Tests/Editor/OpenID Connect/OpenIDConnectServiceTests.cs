487983213ef25339581d6f7ae32658e952cefc48
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-07-22 10:50:27 +0200

    added unit tests for oidc service

--- Assets/i5 Toolkit for Unity/Tests/Editor/OpenID Connect/OpenIDConnectServiceTests.cs
+++ Assets/i5 Toolkit for Unity/Tests/Editor/OpenID Connect/OpenIDConnectServiceTests.cs
@@ -1,5 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using FakeItEasy;
+using i5.Toolkit.Core.OpenIDConnectClient;
+using i5.Toolkit.Core.ServiceCore;
+using i5.Toolkit.Core.TestUtilities;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
@@ -8,6 +13,86 @@ namespace i5.Toolkit.Core.Tests.OpenIDConnectClient
 {
     public class OpenIDConnectServiceTests
     {
+        /// <summary>
+        /// Resets the scene to the standard test scene before executed each test
+        /// </summary>
+        [SetUp]
+        public void ResetScene()
+        {
+            EditModeTestUtilities.ResetScene();
+        }
 
+        [Test]
+        public void ClientDataLoader_Initialized_DefaultNotNull()
+        {
+            OpenIDConnectService oidc = new OpenIDConnectService();
+            Assert.NotNull(oidc.ClientDataLoader);
+        }
+
+        [Test]
+        public void Initialize_Initialized_LoadsClientData()
+        {
+            OpenIDConnectService oidc = new OpenIDConnectService();
+            IClientDataLoader dataLoader = A.Fake<IClientDataLoader>();
+            A.CallTo(() => dataLoader.LoadClientDataAsync()).Returns(Task.FromResult(A.Fake<ClientData>()));
+            BaseServiceManager serviceManager = A.Fake<BaseServiceManager>();
+            oidc.ClientDataLoader = dataLoader;
+
+            oidc.Initialize(serviceManager);
+
+            A.CallTo(() => oidc.ClientDataLoader.LoadClientDataAsync()).MustHaveHappened(Repeated.Once);
+        }
+
+        [Test]
+        public void OpenLoginPage_Called_OpensLoginPage()
+        {
+            OpenIDConnectService oidc = new OpenIDConnectService();
+            IOidcProvider provider = A.Fake<IOidcProvider>();
+            oidc.OidcProvider = provider;
+            oidc.OpenLoginPage();
+            A.CallTo(() => provider.OpenLoginPage(A<string[]>.Ignored, A<string>.Ignored)).MustHaveHappened();
+
+            oidc.StopServerImmediately();
+        }
+
+        [Test]
+        public void Logout_AccessTokenSet_AccessTokenCleared()
+        {
+            OpenIDConnectService oidc = new OpenIDConnectService();
+            typeof(OpenIDConnectService).GetProperty("AccessToken").SetValue(oidc, "abcd");
+            Assert.IsNotEmpty(oidc.AccessToken);
+
+            oidc.Logout();
+            Assert.IsEmpty(oidc.AccessToken);
+        }
+
+        [Test]
+        public void Logout_EventSubscribed_EventRaised()
+        {
+            OpenIDConnectService oidc = new OpenIDConnectService();
+            int events = 0;
+            oidc.LogoutCompleted += delegate
+            {
+                events++;
+            };
+
+            oidc.Logout();
+            Assert.AreEqual(1, events);
+        }
+
+        [Test]
+        public void IsLoggedIn_AccessTokenNull_ReturnsFalse()
+        {
+            OpenIDConnectService oidc = new OpenIDConnectService();
+            Assert.IsFalse(oidc.IsLoggedIn);
+        }
+
+        [Test]
+        public void IsLoggedIn_AccessTokenNotEmpty_ReturnsTrue()
+        {
+            OpenIDConnectService oidc = new OpenIDConnectService();
+            typeof(OpenIDConnectService).GetProperty("AccessToken").SetValue(oidc, "abcd");
+            Assert.IsTrue(oidc.IsLoggedIn);
+        }
     }
 }

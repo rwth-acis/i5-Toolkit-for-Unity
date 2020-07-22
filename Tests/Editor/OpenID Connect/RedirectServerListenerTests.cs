f45101d8bcd14f3ccf4e26f3bd1283fd52f38848
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-07-22 17:00:48 +0200

    added tests for redirect server listener

---
+++ Tests/Editor/OpenID Connect/RedirectServerListenerTests.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using i5.Toolkit.Core.OpenIDConnectClient;
+using i5.Toolkit.Core.TestUtilities;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace i5.Toolkit.Core.Tests.OpenIDConnectClient
+{
+    public class RedirectServerListenerTests
+    {
+        /// <summary>
+        /// Resets the scene to the standard test scene before executed each test
+        /// </summary>
+        [SetUp]
+        public void ResetScene()
+        {
+            EditModeTestUtilities.ResetScene();
+        }
+
+        [Test]
+        public void StartServer_ServerOffline_ServerActiveTrue()
+        {
+            RedirectServerListener rsl = new RedirectServerListener();
+
+            rsl.StartServer();
+
+            Assert.IsTrue(rsl.ServerActive);
+        }
+
+        [Test]
+        public void StartServer_ServerAlreadyRunning_WarningLogged()
+        {
+            RedirectServerListener rsl = new RedirectServerListener();
+            rsl.StartServer();
+
+            LogAssert.Expect(LogType.Warning, new Regex(@"\w*Server is already running\w*"));
+
+            rsl.StartServer();
+        }
+
+        [Test]
+        public void StopServerImmediately_ServerOffline_WarningLogged()
+        {
+            RedirectServerListener rsl = new RedirectServerListener();
+
+            LogAssert.Expect(LogType.Warning, new Regex(@"\w*Server is already stopped\w*"));
+
+            rsl.StopServerImmediately();
+        }
+
+        [Test]
+        public void StopServerImmediately_ServerRunning_ServerActiveFalse()
+        {
+            RedirectServerListener rsl = new RedirectServerListener();
+            rsl.StartServer();
+
+            rsl.StopServerImmediately();
+            Assert.IsFalse(rsl.ServerActive);
+        }
+
+        [Test]
+        public void GenerateRedirectUri_DefaultSchema_StoresRedirectUri()
+        {
+            RedirectServerListener rsl = new RedirectServerListener();
+
+            string generatedUri = rsl.GenerateRedirectUri();
+
+            Assert.AreEqual(generatedUri, rsl.RedirectUri);
+        }
+
+        [Test]
+        public void GenerateRedirectUri_DefaultSchema_UsesHttpSchema()
+        {
+            RedirectServerListener rsl = new RedirectServerListener();
+
+            string generatedUri = rsl.GenerateRedirectUri();
+
+            Assert.IsTrue(generatedUri.StartsWith("http://"));
+        }
+
+        [Test]
+        public void GenerateRedirectUri_CustomSchema_UsesCustomSchema()
+        {
+            RedirectServerListener rsl = new RedirectServerListener();
+
+            string generatedUri = rsl.GenerateRedirectUri("test");
+
+            Assert.IsTrue(generatedUri.StartsWith("test://"));
+        }
+
+        [Test]
+        public void GenerateRedirectUri_DefaultSchema_ContainsPort()
+        {
+            RedirectServerListener rsl = new RedirectServerListener();
+
+            string generatedUri = rsl.GenerateRedirectUri();
+
+            generatedUri = generatedUri.Replace("http://", "");
+
+            Assert.IsTrue(generatedUri.Contains(":"));
+        }
+    }
+}

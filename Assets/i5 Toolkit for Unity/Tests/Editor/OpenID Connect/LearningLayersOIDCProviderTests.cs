3e88b679ed37d42e0f1d52d3eec33735c5c6cdec
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-07-27 11:39:01 +0200

    completed unit tests for learning layers OIDC client

--- Assets/i5 Toolkit for Unity/Tests/Editor/OpenID Connect/LearningLayersOIDCProviderTests.cs
+++ Assets/i5 Toolkit for Unity/Tests/Editor/OpenID Connect/LearningLayersOIDCProviderTests.cs
@@ -45,7 +45,7 @@ namespace i5.Toolkit.Core.Tests.OpenIDConnectClient
 
             string res = task.Result;
 
-            Assert.AreEqual("", res);
+            Assert.IsEmpty(res);
         }
 
         [UnityTest]
@@ -90,7 +90,7 @@ namespace i5.Toolkit.Core.Tests.OpenIDConnectClient
 
             string res = task.Result;
 
-            Assert.AreEqual("", res);
+            Assert.IsEmpty(res);
         }
 
         [Test]
@@ -115,30 +115,204 @@ namespace i5.Toolkit.Core.Tests.OpenIDConnectClient
 
             string res = lloidc.GetAccessToken(redirectParameters);
 
-            Assert.AreEqual("", res);
+            Assert.IsEmpty(res);
         }
 
-        //[UnityTest]
-        //public IEnumerator GetUserInfoAsync_WebResponseSuccessful_ReturnsUserInfo()
-        //{
-        //    LearningLayersOIDCProvider lloidc = new LearningLayersOIDCProvider();
-        //    IRestConnector restConnector = A.Fake<IRestConnector>();
-        //    A.CallTo(() => restConnector.GetAsync(A<string>.Ignored))
-        //        .Returns(new WebResponse<string>("answer", null, 200));
-        //    lloidc.RestConnector = restConnector;
-        //    LearningLayersUserInfo userInfo = new LearningLayersUserInfo("tester", "tester@test.com", "Tester");
-        //    IJsonSerializer serializer = A.Fake<IJsonSerializer>();
-        //    A.CallTo(() => serializer.FromJson<object>(A<string>.Ignored))
-        //        .Returns(userInfo);
-        //    lloidc.JsonSerializer = serializer;
+        [UnityTest]
+        public IEnumerator GetUserInfoAsync_WebResponseSuccessful_ReturnsUserInfo()
+        {
+            LearningLayersOIDCProvider lloidc = new LearningLayersOIDCProvider();
+            IRestConnector restConnector = A.Fake<IRestConnector>();
+            A.CallTo(() => restConnector.GetAsync(A<string>.Ignored))
+                .Returns(new WebResponse<string>("answer", null, 200));
+            lloidc.RestConnector = restConnector;
+            LearningLayersUserInfo userInfo = new LearningLayersUserInfo("tester", "tester@test.com", "Tester");
+            IJsonSerializer serializer = A.Fake<IJsonSerializer>();
+            A.CallTo(() => serializer.FromJson<LearningLayersUserInfo>(A<string>.Ignored))
+                .Returns(userInfo);
+            lloidc.JsonSerializer = serializer;
 
-        //    Task<IUserInfo> task = lloidc.GetUserInfoAsync("");
+            Task<IUserInfo> task = lloidc.GetUserInfoAsync("");
 
-        //    yield return AsyncTest.WaitForTask(task);
+            yield return AsyncTest.WaitForTask(task);
 
-        //    IUserInfo res = task.Result;
+            IUserInfo res = task.Result;
 
-        //    Assert.AreEqual(userInfo.Email, res.Email);
-        //}
+            Assert.AreEqual(userInfo.Email, res.Email);
+        }
+
+        [UnityTest]
+        public IEnumerator GetUserInfoAsync_WebResponseFailed_ReturnsNull()
+        {
+            LearningLayersOIDCProvider lloidc = new LearningLayersOIDCProvider();
+            IRestConnector restConnector = A.Fake<IRestConnector>();
+            A.CallTo(() => restConnector.GetAsync(A<string>.Ignored))
+                .Returns(new WebResponse<string>("This is a simulated error", 400));
+            lloidc.RestConnector = restConnector;
+
+            LogAssert.Expect(LogType.Error, new Regex(@"\w*This is a simulated error\w*"));
+
+            Task<IUserInfo> task = lloidc.GetUserInfoAsync("");
+
+            yield return AsyncTest.WaitForTask(task);
+
+            IUserInfo res = task.Result;
+
+            Assert.IsNull(res);
+        }
+
+        [UnityTest]
+        public IEnumerator GetUserInfoAsync_JsonParseFailed_ReturnsNull()
+        {
+            LearningLayersOIDCProvider lloidc = new LearningLayersOIDCProvider();
+            IRestConnector restConnector = A.Fake<IRestConnector>();
+            A.CallTo(() => restConnector.GetAsync(A<string>.Ignored))
+                .Returns(new WebResponse<string>("answer", null, 200));
+            lloidc.RestConnector = restConnector;
+            LearningLayersUserInfo userInfo = new LearningLayersUserInfo("tester", "tester@test.com", "Tester");
+            IJsonSerializer serializer = A.Fake<IJsonSerializer>();
+            A.CallTo(() => serializer.FromJson<LearningLayersUserInfo>(A<string>.Ignored))
+                .Returns(null);
+            lloidc.JsonSerializer = serializer;
+
+            LogAssert.Expect(LogType.Error, new Regex(@"\w*Could not parse user info\w*"));
+
+            Task<IUserInfo> task = lloidc.GetUserInfoAsync("");
+
+            yield return AsyncTest.WaitForTask(task);
+
+            IUserInfo res = task.Result;
+
+            Assert.IsNull(res);
+        }
+
+        [UnityTest]
+        public IEnumerator CheckAccessTokenAsync_WebResponseSuccessful_ReturnsTrue()
+        {
+            LearningLayersOIDCProvider lloidc = new LearningLayersOIDCProvider();
+            IRestConnector restConnector = A.Fake<IRestConnector>();
+            A.CallTo(() => restConnector.GetAsync(A<string>.Ignored))
+                .Returns(new WebResponse<string>("answer", null, 200));
+            lloidc.RestConnector = restConnector;
+            LearningLayersUserInfo userInfo = new LearningLayersUserInfo("tester", "tester@test.com", "Tester");
+            IJsonSerializer serializer = A.Fake<IJsonSerializer>();
+            A.CallTo(() => serializer.FromJson<LearningLayersUserInfo>(A<string>.Ignored))
+                .Returns(userInfo);
+            lloidc.JsonSerializer = serializer;
+
+            Task<bool> task = lloidc.CheckAccessTokenAsync("");
+
+            yield return AsyncTest.WaitForTask(task);
+
+            bool res = task.Result;
+
+            Assert.IsTrue(res);
+        }
+
+        [UnityTest]
+        public IEnumerator CheckAccessTokenAsync_WebResponseFailed_ReturnsFalse()
+        {
+            LearningLayersOIDCProvider lloidc = new LearningLayersOIDCProvider();
+            IRestConnector restConnector = A.Fake<IRestConnector>();
+            A.CallTo(() => restConnector.GetAsync(A<string>.Ignored))
+                .Returns(new WebResponse<string>("This is a simulated error", 400));
+            lloidc.RestConnector = restConnector;
+
+            LogAssert.Expect(LogType.Error, new Regex(@"\w*This is a simulated error\w*"));
+
+            Task<bool> task = lloidc.CheckAccessTokenAsync("");
+
+            yield return AsyncTest.WaitForTask(task);
+
+            bool res = task.Result;
+
+            Assert.IsFalse(res);
+        }
+
+        [Test]
+        public void OpenLoginPage_UriGiven_BrowserOpened()
+        {
+            LearningLayersOIDCProvider lloidc = new LearningLayersOIDCProvider();
+            IBrowser browser = A.Fake<IBrowser>();
+            lloidc.Browser = browser;
+            lloidc.ClientData = A.Fake<ClientData>();
+            string[] testScopes = new string[] { "testScope" };
+
+            lloidc.OpenLoginPage(testScopes, "http://www.test.com");
+
+            A.CallTo(() => browser.OpenURL(A<string>.That.Contains("http://www.test.com"))).MustHaveHappenedOnceExactly();
+            A.CallTo(() => browser.OpenURL(A<string>.That.Contains("testScope"))).MustHaveHappenedOnceExactly();
+        }
+
+        [Test]
+        public void GetAuthorizationCode_CodeProvided_ExtractsCode()
+        {
+            LearningLayersOIDCProvider lloidc = new LearningLayersOIDCProvider();
+            Dictionary<string, string> redirectParameters = new Dictionary<string, string>();
+            redirectParameters.Add("code", "myCode");
+            string res = lloidc.GetAuthorizationCode(redirectParameters);
+
+            Assert.AreEqual("myCode", res);
+        }
+
+        [Test]
+        public void GetAuthorizationCode_CodeNotProvided_ReturnsEmptyString()
+        {
+            LearningLayersOIDCProvider lloidc = new LearningLayersOIDCProvider();
+            Dictionary<string, string> redirectParameters = new Dictionary<string, string>();
+
+            LogAssert.Expect(LogType.Error, new Regex(@"\w*Redirect parameters did not contain authorization code\w*"));
+
+            string res = lloidc.GetAuthorizationCode(redirectParameters);
+
+            Assert.IsEmpty(res);
+        }
+
+        [Test]
+        public void ParametersContainError_NoError_ReturnsFalse()
+        {
+            LearningLayersOIDCProvider lloidc = new LearningLayersOIDCProvider();
+            Dictionary<string, string> redirectParameters = new Dictionary<string, string>();
+
+            bool res = lloidc.ParametersContainError(redirectParameters, out string message);
+
+            Assert.IsFalse(res);
+        }
+
+        [Test]
+        public void ParametersContainError_NoError_ErrorMessageEmpty()
+        {
+            LearningLayersOIDCProvider lloidc = new LearningLayersOIDCProvider();
+            Dictionary<string, string> redirectParameters = new Dictionary<string, string>();
+
+            bool res = lloidc.ParametersContainError(redirectParameters, out string message);
+
+            Assert.IsEmpty(message);
+        }
+
+        [Test]
+        public void ParametersContainError_ErrorGiven_ReturnsTrue()
+        {
+            LearningLayersOIDCProvider lloidc = new LearningLayersOIDCProvider();
+            Dictionary<string, string> redirectParameters = new Dictionary<string, string>();
+            redirectParameters.Add("error", "This is a simulated error");
+
+            bool res = lloidc.ParametersContainError(redirectParameters, out string message);
+
+            Assert.IsTrue(res);
+        }
+
+        [Test]
+        public void ParametersContainError_ErrorGiven_ErrorMessageSet()
+        {
+            LearningLayersOIDCProvider lloidc = new LearningLayersOIDCProvider();
+            Dictionary<string, string> redirectParameters = new Dictionary<string, string>();
+            string errorMsg = "This is a simulated error";
+            redirectParameters.Add("error", errorMsg);
+
+            bool res = lloidc.ParametersContainError(redirectParameters, out string message);
+
+            Assert.AreEqual(errorMsg, message);
+        }
     }
 }

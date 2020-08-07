d6abbcedcf54403564cb1bdf16fab461ed1cec11
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-08-07 19:42:42 +0200

    added ui operator for Unity versions before 2019.3

---
+++ Assets/Examples/UI Operator/UiOperatorLogger.cs
@@ -0,0 +1,22 @@
+﻿using i5.Toolkit.Core.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiOperatorLogger : MonoBehaviour
+{
+    public void OnToggleSwitched()
+    {
+        i5Debug.Log("Toggle switched", this);
+    }
+
+    public void OnInputFieldChanged()
+    {
+        i5Debug.Log("Input Field text changed", this);
+    }
+
+    public void OnInputFieldEndEdit()
+    {
+        i5Debug.Log("Input Field end edit", this);
+    }
+}
---
+++ Assets/Examples/UI Operator/UiOperatorTester.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiOperatorTester : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F5))
+        {
+            InputField input = GameObject.Find("InputField").GetComponent<InputField>();
+            input.text = "hello";
+        }
+    }
+}

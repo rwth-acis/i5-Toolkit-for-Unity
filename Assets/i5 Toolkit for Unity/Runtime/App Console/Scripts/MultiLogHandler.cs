86181c9b7e800bf260c710960857796c7eab6020
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-08-21 19:34:31 +0200

    added ability to have multiple log handlers in parallel

---
+++ Assets/i5 Toolkit for Unity/Runtime/App Console/Scripts/MultiLogHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace i5.Toolkit.Core.AppConsole
+{
+    public class MultiLogHandler : List<ILogHandler>, ILogHandler
+    {
+        public void LogException(Exception exception, UnityEngine.Object context)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                this[i].LogException(exception, context);
+            }
+        }
+
+        public void LogFormat(LogType logType, UnityEngine.Object context, string format, params object[] args)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                this[i].LogFormat(logType, context, format, args);
+            }
+        }
+    }
+}

f993faa55d9b1e93d010570e0e7023b35597a7c8
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-08-22 19:56:21 +0200

    added core console functionality

---
+++ Assets/i5 Toolkit for Unity/Runtime/App Console/Scripts/ILogFormatter.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using UnityEngine;
+
+public interface ILogFormatter
+{
+    string FormatString(Exception exception, UnityEngine.Object context);
+    string FormatString(LogType logType, UnityEngine.Object context, string format, params object[] args);
+}

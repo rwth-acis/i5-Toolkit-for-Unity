590fa5193d3e02a612697e69a918e7b1133562b7
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-08-22 19:56:21 +0200

    added core console functionality

---
+++ Runtime/App Console/Scripts/Console.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Console : IMessageDisplay
+{
+
+
+
+    public void AddMessage(string message)
+    {
+    }
+}
---
+++ Runtime/App Console/Scripts/DefaultLogFormatter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+class DefaultLogFormatter : ILogFormatter
+{
+    public string FormatString(Exception exception, UnityEngine.Object context)
+    {
+        return exception.ToString();
+    }
+
+    public string FormatString(LogType logType, UnityEngine.Object context, string format, params object[] args)
+    {
+        return logType.ToString() + format.ToString();
+    }
+}

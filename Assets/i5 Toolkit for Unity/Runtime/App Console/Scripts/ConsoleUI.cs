91c1857be8f6bbfc52108edc52bb4fd68b1fbf7b
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-08-23 15:17:03 +0200

    connected elements of app console

---
+++ Assets/i5 Toolkit for Unity/Runtime/App Console/Scripts/ConsoleUI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using TMPro;
+
+public class ConsoleUI : MonoBehaviour, IMessageDisplayUI
+{
+    [SerializeField] private TextMeshProUGUI consoleTextDisplay;
+    [SerializeField] private bool autoScroll;
+
+    public void UpdateUI(List<string> messages)
+    {
+        string text = "";
+        for (int i = 0; i < messages.Count; i++)
+        {
+            text += messages[i];
+            if (i < messages.Count - 1)
+            {
+                text += Environment.NewLine;
+            }
+        }
+
+        consoleTextDisplay.text = text;
+    }
+}

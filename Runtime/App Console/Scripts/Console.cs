f1bd722b5eb51681307bef918c6997ab3bc2b59b
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-08-23 15:17:03 +0200

    connected elements of app console

--- Runtime/App Console/Scripts/Console.cs
+++ Runtime/App Console/Scripts/Console.cs
@@ -1,13 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using i5.Toolkit.Core.AppConsole;
 
-public class Console : IMessageDisplay
+public class Console : MonoBehaviour, IMessageDisplay
 {
+    private IMessageDisplayUI consoleUi;
+    private ILogFormatter consoleFormatter;
 
+    private List<string> messages = new List<string>();
 
+    private void Awake()
+    {
+        consoleFormatter = GetComponent<ILogFormatter>();
+        consoleUi = GetComponent<IMessageDisplayUI>();
+    }
+
+    private void Start()
+    {
+        MessageDisplayLogHandler logHandler = new MessageDisplayLogHandler(this, consoleFormatter);
+        Debug.unityLogger.AddLogHandler(logHandler);
+    }
 
     public void AddMessage(string message)
     {
+        messages.Add(message);
+        consoleUi.UpdateUI(messages);
     }
 }

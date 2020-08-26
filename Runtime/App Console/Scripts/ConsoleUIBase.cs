e7fcf80297bcf7f20335cb0a0a5b6bb2e84a07cf
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-08-26 11:23:56 +0200

    added first prototype for console auto scroll

--- Runtime/App Console/Scripts/ConsoleUIBase.cs
+++ Runtime/App Console/Scripts/ConsoleUIBase.cs
@@ -1,9 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class ConsoleUIBase : MonoBehaviour
 {
     [SerializeField] protected ConsoleFormatterBase consoleFormatter;
+    [SerializeField] protected ScrollRect scrollRect;
+    [SerializeField] protected Button autoScrollButton;
+
+    private bool autoScroll;
 
     protected virtual void Awake()
     {
@@ -11,8 +17,42 @@ public abstract class ConsoleUIBase : MonoBehaviour
         {
             consoleFormatter = new DefaultConsoleFormatter();
         }
+        autoScrollButton.onClick.AddListener(OnAutoScrollButtonClicked);
     }
 
-    public abstract void UpdateUI(List<INotificationMessage> notificationMessages);
+    public virtual void UpdateUI(List<INotificationMessage> notificationMessages)
+    {
+        ApplyAutoscroll();
+    }
+
+    public virtual void OnScrollValueChanged(Vector2 value)
+    {
+        //if (!scrollLock && value.y < 1f)
+        //{
+        //    autoScroll = false;
+        //    Debug.Log("no auto scroll anymore");
+        //}
+    }
+
+    public virtual void OnAutoScrollButtonClicked()
+    {
+        autoScroll = true;
+        ApplyAutoscroll();
+    }
+
+    private void ApplyAutoscroll()
+    {
+        if (autoScroll)
+        {
+            StartCoroutine(AutoscrollAfterTMPUpdate());
+        }
+    }
+
+    private IEnumerator AutoscrollAfterTMPUpdate()
+    {
+        // wait one frame so that the TMP can update
+        yield return null;
+        scrollRect.verticalNormalizedPosition = 0;
+    }
 }
 

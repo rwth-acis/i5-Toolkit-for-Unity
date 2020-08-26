b02411682b5234960161888fe21c33d648fde21d
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-08-26 12:46:13 +0200

    implemented auto scroll

---
+++ Runtime/App Console/Scripts/AutoScroll.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutoScroll : MonoBehaviour
+{
+    [SerializeField] protected ScrollRect scrollRect;
+    [SerializeField] protected Button autoScrollButton;
+    [SerializeField] protected TextMeshProUGUI content;
+
+    private bool autoScroll;
+
+    public bool ExpectContentChange { get; set; }
+
+    private void Awake()
+    {
+        autoScrollButton.onClick.AddListener(OnAutoScrollButtonClicked);
+        scrollRect.onValueChanged.AddListener(OnScrollValueChanged);
+    }
+
+    public void OnAutoScrollButtonClicked()
+    {
+        autoScroll = true;
+        ApplyAutoscroll();
+    }
+
+    public void OnScrollValueChanged(Vector2 value)
+    {
+        if (ExpectContentChange)
+        {
+            ExpectContentChange = false;
+            ApplyAutoscroll();
+        }
+        else
+        {
+            autoScroll = value.y <= 0;
+        }
+    }
+
+
+    private void ApplyAutoscroll()
+    {
+        if (autoScroll)
+        {
+            content.ForceMeshUpdate();
+            scrollRect.verticalNormalizedPosition = 0;
+        }
+    }
+}
--- Runtime/App Console/Scripts/ConsoleUIBase.cs
+++ Runtime/App Console/Scripts/ConsoleUIBase.cs
@@ -6,10 +6,7 @@ using UnityEngine.UI;
 public abstract class ConsoleUIBase : MonoBehaviour
 {
     [SerializeField] protected ConsoleFormatterBase consoleFormatter;
-    [SerializeField] protected ScrollRect scrollRect;
-    [SerializeField] protected Button autoScrollButton;
-
-    private bool autoScroll;
+    [SerializeField] protected AutoScroll autoScroll;
 
     protected virtual void Awake()
     {
@@ -17,42 +14,11 @@ public abstract class ConsoleUIBase : MonoBehaviour
         {
             consoleFormatter = new DefaultConsoleFormatter();
         }
-        autoScrollButton.onClick.AddListener(OnAutoScrollButtonClicked);
     }
 
     public virtual void UpdateUI(List<INotificationMessage> notificationMessages)
     {
-        ApplyAutoscroll();
-    }
-
-    public virtual void OnScrollValueChanged(Vector2 value)
-    {
-        //if (!scrollLock && value.y < 1f)
-        //{
-        //    autoScroll = false;
-        //    Debug.Log("no auto scroll anymore");
-        //}
-    }
-
-    public virtual void OnAutoScrollButtonClicked()
-    {
-        autoScroll = true;
-        ApplyAutoscroll();
-    }
-
-    private void ApplyAutoscroll()
-    {
-        if (autoScroll)
-        {
-            StartCoroutine(AutoscrollAfterTMPUpdate());
-        }
-    }
-
-    private IEnumerator AutoscrollAfterTMPUpdate()
-    {
-        // wait one frame so that the TMP can update
-        yield return null;
-        scrollRect.verticalNormalizedPosition = 0;
+        autoScroll.ExpectContentChange = true;
     }
 }
 

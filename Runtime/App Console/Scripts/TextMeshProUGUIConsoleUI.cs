af003f1841f5d9fd378832ef5443ae8446651cb3
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-11-07 17:50:44 +0100

    added wrappers for text meshes; console now works with arbitrary text meshes

--- Runtime/App Console/Scripts/TextMeshProUGUIConsoleUI.cs
+++ Runtime/App Console/Scripts/TextMeshProUGUIConsoleUI.cs
@@ -1,16 +1,18 @@
-﻿using TMPro;
+﻿using i5.Toolkit.Core.Utilities.UnityAdapters;
+using TMPro;
 using UnityEngine;
 
 namespace i5.Toolkit.Core.AppConsole
 {
-    public class TmpConsoleUIBehaviour : ConsoleUIBehaviour
+    public class TextMeshProUGUIConsoleUI : ConsoleUIBehaviour
     {
         [SerializeField] private TextMeshProUGUI consoleTextDisplay;
         [SerializeField] protected LogFormatterConfiguration logFormatterConfiguration;
 
         protected override void Awake()
         {
-            consoleUI = new TmpConsoleUI(consoleTextDisplay, logFormatterConfiguration);
+            ITextAdapter textMeshProUGUIAdapter = new TextMeshProUGUITextAdapter(consoleTextDisplay);
+            consoleUI = new TextConsoleUI(textMeshProUGUIAdapter, logFormatterConfiguration);
 
             base.Awake();
         }
---
+++ Runtime/Utilities/UnityWrappers/TextUI Wrappers/TextMeshProTextAdapter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace i5.Toolkit.Core.Utilities.UnityAdapters
+{
+    public class TextMeshProTextAdapter : ITextAdapter
+    {
+        public TextMeshPro TextMesh { get; private set; }
+
+        public TextMeshProTextAdapter(TextMeshPro textMesh)
+        {
+            TextMesh = textMesh;
+        }
+
+        public string Text
+        {
+            get => TextMesh.text;
+            set => TextMesh.text = value;
+        }
+    }
+}

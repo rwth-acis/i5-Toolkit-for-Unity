17813dc2885afa0e5ce24a99cabfb4a7f02448bb
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-11-24 23:44:45 +0100

    moved classes into namespaces

--- Assets/i5 Toolkit for Unity/Samples/OpenID Connect/OpenIDConnectTester.cs
+++ Assets/i5 Toolkit for Unity/Samples/OpenID Connect/OpenIDConnectTester.cs
@@ -1,45 +1,46 @@
 ﻿using i5.Toolkit.Core.OpenIDConnectClient;
 using i5.Toolkit.Core.ServiceCore;
 using i5.Toolkit.Core.Utilities;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class OpenIDConnectTester : MonoBehaviour
+namespace i5.Toolkit.Core.Examples.OpenIDConnectClient
 {
-    [SerializeField]
-    private ClientDataObject learningLayersClientData;
-
-    private bool isSubscribedToOidc = false;
-
-    // Update is called once per frame
-    private void Update()
+    public class OpenIDConnectTester : MonoBehaviour
     {
-        if (Input.GetKeyDown(KeyCode.F5))
+        [SerializeField]
+        private ClientDataObject learningLayersClientData;
+
+        private bool isSubscribedToOidc = false;
+
+        // Update is called once per frame
+        private void Update()
         {
-            LearningLayersOIDCProvider provider = new LearningLayersOIDCProvider();
-            provider.ClientData = learningLayersClientData.clientData;
-            ServiceManager.GetService<OpenIDConnectService>().OidcProvider = provider;
-
-            // only subscribe to the event if it was not yet done before, e.g. in a failed login attempt
-            if (!isSubscribedToOidc)
+            if (Input.GetKeyDown(KeyCode.F5))
             {
-                ServiceManager.GetService<OpenIDConnectService>().LoginCompleted += OpenIDConnectTester_LoginCompleted;
-                isSubscribedToOidc = true;
+                LearningLayersOIDCProvider provider = new LearningLayersOIDCProvider();
+                provider.ClientData = learningLayersClientData.clientData;
+                ServiceManager.GetService<OpenIDConnectService>().OidcProvider = provider;
+
+                // only subscribe to the event if it was not yet done before, e.g. in a failed login attempt
+                if (!isSubscribedToOidc)
+                {
+                    ServiceManager.GetService<OpenIDConnectService>().LoginCompleted += OpenIDConnectTester_LoginCompleted;
+                    isSubscribedToOidc = true;
+                }
+                ServiceManager.GetService<OpenIDConnectService>().OpenLoginPage();
             }
-            ServiceManager.GetService<OpenIDConnectService>().OpenLoginPage();
         }
-    }
 
-    private async void OpenIDConnectTester_LoginCompleted(object sender, System.EventArgs e)
-    {
-        i5Debug.Log("Login completed", this);
-        i5Debug.Log(ServiceManager.GetService<OpenIDConnectService>().AccessToken, this);
-        ServiceManager.GetService<OpenIDConnectService>().LoginCompleted -= OpenIDConnectTester_LoginCompleted;
-        isSubscribedToOidc = false;
+        private async void OpenIDConnectTester_LoginCompleted(object sender, System.EventArgs e)
+        {
+            i5Debug.Log("Login completed", this);
+            i5Debug.Log(ServiceManager.GetService<OpenIDConnectService>().AccessToken, this);
+            ServiceManager.GetService<OpenIDConnectService>().LoginCompleted -= OpenIDConnectTester_LoginCompleted;
+            isSubscribedToOidc = false;
 
-        IUserInfo userInfo = await ServiceManager.GetService<OpenIDConnectService>().GetUserDataAsync();
-        i5Debug.Log("Currently logged in user: " + userInfo.FullName 
-            + " (username: " + userInfo.Username + ") with the mail address " + userInfo.Email, this);
+            IUserInfo userInfo = await ServiceManager.GetService<OpenIDConnectService>().GetUserDataAsync();
+            i5Debug.Log("Currently logged in user: " + userInfo.FullName
+                + " (username: " + userInfo.Username + ") with the mail address " + userInfo.Email, this);
+        }
     }
 }

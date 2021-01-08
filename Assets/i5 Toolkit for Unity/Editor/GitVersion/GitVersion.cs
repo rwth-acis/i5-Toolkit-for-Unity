07aaec5ff6ca2a009092c8134952b612e984cfc3
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2021-01-08 15:30:00 +0100

    extended versioning functionality

--- Assets/i5 Toolkit for Unity/Editor/GitVersion/GitVersion.cs
+++ Assets/i5 Toolkit for Unity/Editor/GitVersion/GitVersion.cs
@@ -23,7 +23,38 @@ public static class GitVersion
         }
     }
 
-    [MenuItem("i5 Toolkit/Get Semantic Version")]
+    public static bool TryGetBranch(out string branchName)
+    {
+        int resCode = RunGit(@"rev-parse --abbrev-ref HEAD", out string output, out string errors);
+        if (resCode != 0)
+        {
+            UnityEngine.Debug.LogWarning("Error running git: " + errors);
+            branchName = "UNKNOWN";
+            return false;
+        }
+        else
+        {
+            branchName = output;
+            return true;
+        }
+    }
+
+    public static bool TryGetTotalCommitsOnBranch(out int commitCount)
+    {
+        int resCode = RunGit(@"rev-list --count HEAD", out string output, out string errors);
+        if (resCode != 0)
+        {
+            UnityEngine.Debug.LogWarning("Error running git commit counter: " + errors);
+            commitCount = 0;
+            return false;
+        }
+        else
+        {
+            return int.TryParse(output, out commitCount);
+        }
+    }
+
+    [MenuItem("i5 Toolkit/Build Versioning/Get Semantic Version")]
     public static void TestVersion()
     {
         if (TryGetVersion(out string version))
@@ -33,6 +64,32 @@ public static class GitVersion
         else
         {
             UnityEngine.Debug.LogError($"Could not get version. Using default {version}. See previous output for error messages");
+        }
+    }
+
+    [MenuItem("i5 Toolkit/Build Versioning/Get Git Branch")]
+    public static void TestBranch()
+    {
+        if (TryGetBranch(out string branchName))
+        {
+            UnityEngine.Debug.Log("Branch: " + branchName);
+        }
+        else
+        {
+            UnityEngine.Debug.LogError($"Could not get branch. See previous output for error messages");
+        }
+    }
+
+    [MenuItem("i5 Toolkit/Build Versioning/Get Total Commits on Branch")]
+    public static void TestTotalCommits()
+    {
+        if (TryGetTotalCommitsOnBranch(out int commitCount))
+        {
+            UnityEngine.Debug.Log("Total number of commits on branch: " + commitCount);
+        }
+        else
+        {
+            UnityEngine.Debug.LogError("Could not get commit count. See previous output for error messages");
         }
     }
 

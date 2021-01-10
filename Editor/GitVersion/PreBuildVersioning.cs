2ccb4041d5df729e95ecd578c4d48d3ca7429e44
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2021-01-10 13:02:24 +0100

    refactored code and added unit tests

--- Editor/GitVersion/PreBuildVersioning.cs
+++ Editor/GitVersion/PreBuildVersioning.cs
@@ -4,73 +4,78 @@ using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
 
-public class PreBuildVersioning : IPreprocessBuildWithReport
+namespace i5.Toolkit.Core.GitVersion
 {
-    private const string toolName = "i5 Build Versioning Tool";
-    private const string gitVersionplaceholder = "$gitVersion";
-    private const string branchPlaceholder = "$branch";
-
-    public int callbackOrder => 0;
-
-    public void OnPreprocessBuild(BuildReport report)
+    public class PreBuildVersioning : IPreprocessBuildWithReport
     {
-        string versionString = PlayerSettings.bundleVersion;
+        private const string toolName = "i5 Build Versioning Tool";
+        private const string gitVersionplaceholder = "$gitVersion";
+        private const string branchPlaceholder = "$branch";
 
-        if (!versionString.Contains(gitVersionplaceholder))
+        public int callbackOrder => 0;
+
+        public void OnPreprocessBuild(BuildReport report)
         {
-            Debug.Log($"[{toolName}] Version placeholder not found. To use automatic semantic versioning with Git, write the placeholder {gitVersionplaceholder} into the application's version");
-        }
-        else
-        {
-            Debug.Log($"[{toolName}] Version placeholder found. Running versioning tool to calculate semantic version number from Git tags");
-            if (!GitVersion.TryGetVersion(out string version))
+            string versionString = PlayerSettings.bundleVersion;
+
+            GitVersionCalculator gitVersion = new GitVersionCalculator();
+
+            if (!versionString.Contains(gitVersionplaceholder))
             {
-                Debug.LogWarning($"[{toolName}] Could not get version name. Version placeholder will be replaced with default {version}");
+                Debug.Log($"[{toolName}] Version placeholder not found. To use automatic semantic versioning with Git, write the placeholder {gitVersionplaceholder} into the application's version");
             }
-
-            versionString = versionString.Replace(gitVersionplaceholder, version);
-        }
-
-        if (versionString.Contains(branchPlaceholder))
-        {
-            Debug.Log($"[{toolName}] Branch placeholder found. Running git to get the branch name");
-            if (!GitVersion.TryGetBranch(out string branch))
+            else
             {
-                Debug.LogWarning($"[{toolName}] Could not get branch name. Branch placeholder will be replaced with UNKNOWN");
-            }
-
-            versionString = versionString.Replace(branchPlaceholder, branch);
-        }
-
-        PlayerSettings.bundleVersion = versionString;
-        Version packageVersion = StringToVersion(PlayerSettings.bundleVersion);
-        PlayerSettings.WSA.packageVersion = packageVersion;
-        if (GitVersion.TryGetTotalCommitsOnBranch(out int commitCount))
-        {
-            PlayerSettings.Android.bundleVersionCode = commitCount;
-        }
-    }
-
-    private Version StringToVersion(string strVersion)
-    {
-        string[] versionFragments = strVersion.Split('.');
-        int[] intVersion = new int[3];
-        int index = 0;
-        foreach (string fragment in versionFragments)
-        {
-            if (int.TryParse(fragment, out int intFragment))
-            {
-                if (index < 3)
+                Debug.Log($"[{toolName}] Version placeholder found. Running versioning tool to calculate semantic version number from Git tags");
+                if (!gitVersion.TryGetVersion(out string version))
                 {
-                    intVersion[index] = intFragment;
-                    index++;
+                    Debug.LogWarning($"[{toolName}] Could not get version name. Version placeholder will be replaced with default {version}");
                 }
-                else
+
+                versionString = versionString.Replace(gitVersionplaceholder, version);
+            }
+
+            if (versionString.Contains(branchPlaceholder))
+            {
+                Debug.Log($"[{toolName}] Branch placeholder found. Running git to get the branch name");
+                if (!gitVersion.TryGetBranch(out string branch))
                 {
-                    Debug.LogWarning($"[{toolName}] Version includes too many numbers");
+                    Debug.LogWarning($"[{toolName}] Could not get branch name. Branch placeholder will be replaced with UNKNOWN");
                 }
+
+                versionString = versionString.Replace(branchPlaceholder, branch);
+            }
+
+            PlayerSettings.bundleVersion = versionString;
+            Version packageVersion = StringToVersion(PlayerSettings.bundleVersion);
+            PlayerSettings.WSA.packageVersion = packageVersion;
+            if (gitVersion.TryGetTotalCommitsOnBranch(out int commitCount))
+            {
+                PlayerSettings.Android.bundleVersionCode = commitCount;
             }
         }
-        return new Version(intVersion[0], intVersion[1], intVersion[2], 0);
+
+        private Version StringToVersion(string strVersion)
+        {
+            string[] versionFragments = strVersion.Split('.');
+            int[] intVersion = new int[3];
+            int index = 0;
+            foreach (string fragment in versionFragments)
+            {
+                if (int.TryParse(fragment, out int intFragment))
+                {
+                    if (index < 3)
+                    {
+                        intVersion[index] = intFragment;
+                        index++;
+                    }
+                    else
+                    {
+                        Debug.LogWarning($"[{toolName}] Version includes too many numbers");
+                    }
+                }
+            }
+            return new Version(intVersion[0], intVersion[1], intVersion[2], 0);
+        }
     }
 }

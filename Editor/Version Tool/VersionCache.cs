bfcce9da6d4ea6a4bdaca611b8ced784fe15c41a
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2021-01-13 23:21:16 +0100

    more stable versioning for WSA, more documentation

--- Editor/Version Tool/VersionCache.cs
+++ Editor/Version Tool/VersionCache.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 
 namespace i5.Toolkit.Core.VersionTool
 {
+    /// <summary>
+    /// Cache for the version settings so that they can be restored after the build
+    /// </summary>
     [Serializable]
     public class VersionCache
     {
+        /// <summary>
+        /// Version string which is used on many platforms, e.g. Standalone
+        /// </summary>
         public string appVersion;
+        /// <summary>
+        /// Version for WSA apps
+        /// </summary>
         public Version wsaVersion;
+        /// <summary>
+        /// Android version number
+        /// </summary>
         public int androidVersion;
 
+        // Unity cannot serialize the Version object,
+        // so it is first converted to a string that is serialized
         [SerializeField] private string strWsaVersion;
 
+        // path where the cache is stored
         private const string savePath = "Temp/VersionCache.tmp";
 
+        /// <summary>
+        /// Saves the cache as a temporary file in the project's Temp folder
+        /// </summary>
         public void Save()
         {
             strWsaVersion = wsaVersion.ToString();
@@ -22,6 +40,11 @@ namespace i5.Toolkit.Core.VersionTool
             File.WriteAllText(savePath, json);
         }
 
+        /// <summary>
+        /// Loads the cache if it exists
+        /// If no cache file exists, default values are loaded
+        /// </summary>
+        /// <returns>Returns the loaded version cache object</returns>
         public static VersionCache Load()
         {
             if (!File.Exists(savePath))
@@ -34,6 +57,9 @@ namespace i5.Toolkit.Core.VersionTool
             return versionCache;
         }
 
+        /// <summary>
+        /// Clears the cache by deleting the temporary file
+        /// </summary>
         public static void Remove()
         {
             File.Delete(savePath);

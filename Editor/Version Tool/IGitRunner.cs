7944c3d069afdf45b4fc9e0bc424d7b6aa001382
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2021-01-13 21:20:03 +0100

    removed unnecessary using directives

--- Editor/Version Tool/IGitRunner.cs
+++ Editor/Version Tool/IGitRunner.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace i5.Toolkit.Core.VersionTool
+﻿namespace i5.Toolkit.Core.VersionTool
 {
     /// <summary>
     /// Bridge to git's command line interface

4c35d8e7fbff336ef59137df988dfb5ab069cb27
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2021-01-30 18:01:12 +0100

    added unit tests for app version placeholder and android env var

--- Assets/i5 Toolkit for Unity/Tests/Editor/Version Tool/GitVersionBuildStepTests.cs
+++ Assets/i5 Toolkit for Unity/Tests/Editor/Version Tool/GitVersionBuildStepTests.cs
@@ -1,5 +1,6 @@
 ﻿using FakeItEasy;
 using i5.Toolkit.Core.Editor.TestHelpers;
+using i5.Toolkit.Core.Utilities.SystemAdapters;
 using i5.Toolkit.Core.VersionTool;
 using NUnit.Framework;
 using System;
@@ -18,9 +19,11 @@ namespace i5.Toolkit.Core.Tests.VersionTool
         }
 
         [Test]
-        public void ContainsPlaceholders_ContainsVersion_ReturnsTrue()
+        public void ContainsPlaceholders_ContainsGitVersion_ReturnsTrue()
         {
-            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(out IGitVersionCalculator versionCalculator);
+            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(
+                out IGitVersionCalculator versionCalculator,
+                out ISystemEnvironment systemEnvironment);
 
             Assert.IsTrue(buildStep.ContainsPlaceholder("$gitVersion"));
         }
@@ -28,15 +31,29 @@ namespace i5.Toolkit.Core.Tests.VersionTool
         [Test]
         public void ContainsPlaceholders_ContainsBranch_ReturnsTrue()
         {
-            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(out IGitVersionCalculator versionCalculator);
+            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(
+                out IGitVersionCalculator versionCalculator,
+                out ISystemEnvironment systemEnvironment);
 
             Assert.IsTrue(buildStep.ContainsPlaceholder("$gitBranch"));
         }
 
         [Test]
+        public void ContainsPlaceholders_ContainsAppVersion_ReturnsTrue()
+        {
+            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(
+                out IGitVersionCalculator versionCalculator,
+                out ISystemEnvironment systemEnvironment);
+
+            Assert.IsTrue(buildStep.ContainsPlaceholder("$appVersion"));
+        }
+
+        [Test]
         public void ContainsPlaceholders_NoPlaceholders_ReturnsFalse()
         {
-            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(out IGitVersionCalculator versionCalculator);
+            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(
+                out IGitVersionCalculator versionCalculator,
+                out ISystemEnvironment systemEnvironment);
 
             Assert.IsFalse(buildStep.ContainsPlaceholder("$noPlaceholder"));
         }
@@ -44,15 +61,20 @@ namespace i5.Toolkit.Core.Tests.VersionTool
         [Test]
         public void ContainsPlaceholders_Empty_ReturnsFalse()
         {
-            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(out IGitVersionCalculator versionCalculator);
+            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(
+                out IGitVersionCalculator versionCalculator,
+                out ISystemEnvironment systemEnvironment);
 
             Assert.IsFalse(buildStep.ContainsPlaceholder(""));
         }
 
         [Test]
-        public void ReplacePlaceholders_ContainsVersionPlaceholder_Replaces()
+        public void ReplacePlaceholders_ContainsGitVersionPlaceholder_Replaces()
         {
-            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(out IGitVersionCalculator versionCalculator);
+            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(
+                out IGitVersionCalculator versionCalculator,
+                out ISystemEnvironment systemEnvironment);
+
             string ignored = null;
             A.CallTo(()
                 => versionCalculator
@@ -68,7 +90,10 @@ namespace i5.Toolkit.Core.Tests.VersionTool
         [Test]
         public void ReplacePlaceholders_ContainsVersionTwice_ReplacesBoth()
         {
-            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(out IGitVersionCalculator versionCalculator);
+            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(
+                out IGitVersionCalculator versionCalculator,
+                out ISystemEnvironment systemEnvironment);
+
             string ignored = null;
             A.CallTo(()
                 => versionCalculator
@@ -84,7 +109,10 @@ namespace i5.Toolkit.Core.Tests.VersionTool
         [Test]
         public void ReplacePlaceholders_ContainsNothing_NoModifications()
         {
-            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(out IGitVersionCalculator versionCalculator);
+            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(
+                out IGitVersionCalculator versionCalculator,
+                out ISystemEnvironment systemEnvironment);
+
             string ignored = null;
             A.CallTo(()
                 => versionCalculator
@@ -100,7 +128,10 @@ namespace i5.Toolkit.Core.Tests.VersionTool
         [Test]
         public void ReplacePlaceholders_ContainsBranch_ReplacesBranch()
         {
-            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(out IGitVersionCalculator versionCalculator);
+            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(
+                out IGitVersionCalculator versionCalculator,
+                out ISystemEnvironment systemEnvironment);
+
             string ignored = null;
             A.CallTo(()
                 => versionCalculator.TryGetBranch(out ignored))
@@ -115,7 +146,10 @@ namespace i5.Toolkit.Core.Tests.VersionTool
         [Test]
         public void ReplacePlaceholders_ContainsVersionAndBranch_ReplacesBoth()
         {
-            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(out IGitVersionCalculator versionCalculator);
+            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(
+                out IGitVersionCalculator versionCalculator,
+                out ISystemEnvironment systemEnvironment);
+
             string ignored = null;
             A.CallTo(()
                 => versionCalculator.TryGetVersion(out ignored))
@@ -132,9 +166,60 @@ namespace i5.Toolkit.Core.Tests.VersionTool
         }
 
         [Test]
+        public void ReplacePlaceholders_AppVersion_EnvVarSet_UsesEnvVar()
+        {
+            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(
+                out IGitVersionCalculator versionCalculator,
+                out ISystemEnvironment systemEnvironment);
+
+            string ignored = null;
+            A.CallTo(()
+                => versionCalculator
+                .TryGetVersion(out ignored))
+                .Returns(true)
+                .AssignsOutAndRefParameters("1.1.0");
+
+            A.CallTo(()
+                => systemEnvironment
+                .GetEnvironmentVariable(A<string>.Ignored))
+                .Returns("1.2.3");
+
+            string result = buildStep.ReplacePlaceholders("prefix$appVersion-postfix");
+
+            Assert.AreEqual("prefix1.2.3-postfix", result);
+        }
+
+        [Test]
+        public void ReplacePlaceholders_AppVersion_EnvVarNotSet_UsesGit()
+        {
+            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(
+                out IGitVersionCalculator versionCalculator,
+                out ISystemEnvironment systemEnvironment);
+
+            string ignored = null;
+            A.CallTo(()
+                => versionCalculator
+                .TryGetVersion(out ignored))
+                .Returns(true)
+                .AssignsOutAndRefParameters("1.2.3");
+
+            A.CallTo(()
+                => systemEnvironment
+                .GetEnvironmentVariable(A<string>.Ignored))
+                .Returns("");
+
+            string result = buildStep.ReplacePlaceholders("prefix$appVersion-postfix");
+
+            Assert.AreEqual("prefix1.2.3-postfix", result);
+        }
+
+        [Test]
         public void WSAVersion_IsolatedVersionGiven_ReturnsVersion()
         {
-            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(out IGitVersionCalculator versionCalculator);
+            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(
+                out IGitVersionCalculator versionCalculator,
+                out ISystemEnvironment systemEnvironment);
+
             string ignored = null;
             A.CallTo(() => versionCalculator.TryGetVersion(out ignored))
                 .Returns(true)
@@ -149,9 +234,12 @@ namespace i5.Toolkit.Core.Tests.VersionTool
         }
 
         [Test]
-        public void AndroidVersion_UsesCommitCount()
+        public void AndroidVersion_EnvVarNotSet_UsesCommitCount()
         {
-            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(out IGitVersionCalculator versionCalculator);
+            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(
+                out IGitVersionCalculator versionCalculator,
+                out ISystemEnvironment systemEnvironment);
+
             int ignored = 0;
             A.CallTo(() 
                 => versionCalculator
@@ -159,21 +247,84 @@ namespace i5.Toolkit.Core.Tests.VersionTool
                 .Returns(true)
                 .AssignsOutAndRefParameters(123);
 
+            A.CallTo(()
+                => systemEnvironment
+                .GetEnvironmentVariable(A<string>.Ignored))
+                .Returns("");
+
             int result = buildStep.AndroidVersion;
 
             Assert.AreEqual(123, result);
         }
 
-        private GitVersionBuildStep CreateGitVersionBuildStep(out IGitVersionCalculator fakeGitVersion)
+        [Test]
+        public void AndroidVersion_EnvVarSet_UsesEnvVar()
+        {
+            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(
+                out IGitVersionCalculator versionCalculator,
+                out ISystemEnvironment systemEnvironment);
+
+            int ignored = 0;
+            A.CallTo(()
+                => versionCalculator
+                .TryGetTotalCommitsOnBranch(out ignored))
+                .Returns(true)
+                .AssignsOutAndRefParameters(42);
+
+            A.CallTo(()
+                => systemEnvironment
+                .GetEnvironmentVariable(A<string>.Ignored))
+                .Returns("123");
+
+            int result = buildStep.AndroidVersion;
+
+            Assert.AreEqual(123, result);
+        }
+
+        [Test]
+        public void AndroidVersion_EnvVarSetWrong_UsesCommitCount()
+        {
+            GitVersionBuildStep buildStep = CreateGitVersionBuildStep(
+                out IGitVersionCalculator versionCalculator,
+                out ISystemEnvironment systemEnvironment);
+
+            int ignored = 0;
+            A.CallTo(()
+                => versionCalculator
+                .TryGetTotalCommitsOnBranch(out ignored))
+                .Returns(true)
+                .AssignsOutAndRefParameters(123);
+
+            A.CallTo(()
+                => systemEnvironment
+                .GetEnvironmentVariable(A<string>.Ignored))
+                .Returns("test");
+
+            int result = buildStep.AndroidVersion;
+
+            Assert.AreEqual(123, result);
+        }
+
+        private GitVersionBuildStep CreateGitVersionBuildStep(
+            out IGitVersionCalculator fakeGitVersion,
+            out ISystemEnvironment fakeEnvironment)
         {
             GitVersionBuildStep buildStep = new GitVersionBuildStep();
             fakeGitVersion = A.Fake<IGitVersionCalculator>();
+            fakeEnvironment = A.Fake<ISystemEnvironment>();
             buildStep.GetType()
                 .GetField(
                 "gitVersion",
                 System.Reflection.BindingFlags.NonPublic
                 | System.Reflection.BindingFlags.Instance)
                 .SetValue(buildStep, fakeGitVersion);
+
+            buildStep.GetType()
+                .GetField(
+                "systemEnvironment",
+                System.Reflection.BindingFlags.NonPublic
+                | System.Reflection.BindingFlags.Instance)
+                .SetValue(buildStep, fakeEnvironment);
             return buildStep;
         }
     }

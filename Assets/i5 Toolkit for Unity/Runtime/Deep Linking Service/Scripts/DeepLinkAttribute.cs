fb5af290eec3754ed63d129d3f744930804b1821
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2021-03-20 00:25:51 +0100

    added base infrastructure for deep linking service

---
+++ Assets/i5 Toolkit for Unity/Runtime/Deep Linking Service/Scripts/DeepLinkAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+public class DeepLinkAttribute : Attribute
+{
+    public string Path { get; set; }
+
+    public DeepLinkAttribute(string path)
+    {
+        Path = path;
+    }
+}
+
---
+++ Assets/i5 Toolkit for Unity/Runtime/Deep Linking Service/Scripts/DeepLinkingService.cs
@@ -0,0 +1,65 @@
+﻿using i5.Toolkit.Core.ServiceCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+public class DeepLinkingService : IService
+{
+    private List<object> registeredListeners = new List<object>();
+
+    private Dictionary<string, Tuple<object, MethodInfo>> paths = new Dictionary<string, Tuple<object, MethodInfo>>();
+
+    public void Initialize(IServiceManager owner)
+    {
+        Application.deepLinkActivated += OnDeepLinkActivated;
+        if (!string.IsNullOrEmpty(Application.absoluteURL))
+        {
+            OnDeepLinkActivated(Application.absoluteURL);
+        }
+
+        ConstructMapping();
+    }
+
+    private void ConstructMapping()
+    {
+        foreach (object listener in registeredListeners)
+        {
+            MethodInfo[] methods = listener.GetType().GetMethods()
+                .Where(m => m.GetCustomAttributes(typeof(DeepLinkAttribute), false).Length > 0)
+                .ToArray();
+
+            foreach (MethodInfo info in methods)
+            {
+                Attribute[] attributes = info.GetCustomAttributes(typeof(DeepLinkAttribute)).ToArray();
+                foreach (DeepLinkAttribute attribute in attributes)
+                {
+                    paths.Add(attribute.Path, new Tuple<object, MethodInfo>(listener, info));
+                }
+            }
+        }
+    }
+
+    public void Cleanup()
+    {
+        Application.deepLinkActivated += OnDeepLinkActivated;
+    }
+
+    public void AddListenerClass(object obj)
+    {
+        registeredListeners.Add(obj);
+    }
+
+    public void RemoveListenerClass(object obj)
+    {
+        registeredListeners.Remove(obj);
+    }
+
+    public void OnDeepLinkActivated(string path)
+    {
+        // extract path and attributes
+        paths[path].Item2.Invoke(paths[path].Item1, new object[0]);
+    }
+}

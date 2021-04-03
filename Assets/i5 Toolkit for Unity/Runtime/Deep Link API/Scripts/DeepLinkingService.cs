761aec5c09d40e910ff714393789ace2d4bce28d
Benedikt Hensen <hensen@dbis.rwth-aachen.de>
2021-04-03 16:26:42 +0200

    added documentation

--- Assets/i5 Toolkit for Unity/Runtime/Deep Link API/Scripts/DeepLinkingService.cs
+++ Assets/i5 Toolkit for Unity/Runtime/Deep Link API/Scripts/DeepLinkingService.cs
@@ -9,14 +9,32 @@ using UnityEngine;
 
 namespace i5.Toolkit.Core.DeepLinkAPI
 {
+    /// <summary>
+    /// Service that allows deep links to access the application
+    /// Use <see cref="DeepLinkAttribute"/> to mark methods as targets of deep links.
+    /// Moreover, add classes using <see cref="DeepLinkAttribute"/> to the list of listeners in this service using <see cref="AddDeepLinkListener(object)"/>.
+    /// </summary>
     public class DeepLinkingService : IService
     {
+        // registered listeners which should be searched for deep link targets
         private List<WeakReference<object>> registeredListeners = new List<WeakReference<object>>();
 
+        /// <summary>
+        /// Reference to an application wrapper.
+        /// In production, this is initialized with Unity's <see cref="Application"/>.
+        /// </summary>
         public IApplication ApplicationAPI { get; set; } = new ApplicationWrapper();
 
+        /// <summary>
+        /// The number of objects that will be searched for deep link targets
+        /// </summary>
         public int RegisteredListenersCount => registeredListeners.Count;
 
+        /// <summary>
+        /// Initializes the service.
+        /// Subscribes to the deep link event and checks if a deep link already occurred.
+        /// </summary>
+        /// <param name="owner">The service manager that owns this service</param>
         public void Initialize(IServiceManager owner)
         {
             ApplicationAPI.DeepLinkActivated += OnDeepLinkActivated;
@@ -26,10 +44,15 @@ namespace i5.Toolkit.Core.DeepLinkAPI
             }
         }
 
+        /// <summary>
+        /// Adds the given object instance to the list of objects that are scanned for deep link targets
+        /// Add every class that has a method with the <see cref="DeepLinkAttribute"/> so that it can be found.
+        /// </summary>
+        /// <param name="listener">The object to add the the list of listeners</param>
         public void AddDeepLinkListener(object listener)
         {
             // check if list already contains object
-            for (int i=registeredListeners.Count-1;i>=0;i--)
+            for (int i = registeredListeners.Count - 1; i >= 0; i--)
             {
                 if (registeredListeners[i].TryGetTarget(out object item))
                 {
@@ -47,14 +70,20 @@ namespace i5.Toolkit.Core.DeepLinkAPI
             }
 
             registeredListeners.Add(new WeakReference<object>(listener));
+            // check if the listener targets a deep link that already occurred
             if (!string.IsNullOrEmpty(ApplicationAPI.AbsoluteURL))
             {
                 CheckInstanceMethods(ApplicationAPI.AbsoluteURL, listener);
             }
         }
 
+        /// <summary>
+        /// Removes the instance from the list of listeners that will be scanned
+        /// </summary>
+        /// <param name="listener">The object instance to remove from the listener list</param>
         public void RemoveDeepLinkListener(object listener)
         {
+            // find the listener instance and clean up along the way
             for (int i = registeredListeners.Count - 1; i >= 0; i--)
             {
                 if (registeredListeners[i].TryGetTarget(out object item))
@@ -71,16 +100,22 @@ namespace i5.Toolkit.Core.DeepLinkAPI
             }
         }
 
+        /// <summary>
+        /// Cleans up the service
+        /// </summary>
         public void Cleanup()
         {
             ApplicationAPI.DeepLinkActivated -= OnDeepLinkActivated;
             registeredListeners.Clear();
         }
 
+        // Called if a deep link was found
         private void OnDeepLinkActivated(object sender, string deepLink)
         {
             Debug.Log("Got deep link for " + deepLink);
 
+            // go over every registered listener instance and check if it contains a target method
+            // clean up garbage collected references along the way
             for (int i = registeredListeners.Count - 1; i >= 0; i--)
             {
                 if (registeredListeners[i].TryGetTarget(out object listener))
@@ -94,12 +129,14 @@ namespace i5.Toolkit.Core.DeepLinkAPI
             }
         }
 
+        // scans the methods of the given instance's class to find deep link targets
         private void CheckInstanceMethods(string deepLink, object instance)
         {
             Uri uri = new Uri(deepLink);
 
             string path = uri.Authority.ToLower();
 
+            // scan every method that is marked with a DeepLinkAttribute
             MethodInfo[] methods = instance.GetType().GetMethods()
                         .Where(m => m.GetCustomAttributes(typeof(DeepLinkAttribute), false).Length > 0)
                         .ToArray();
@@ -107,12 +144,14 @@ namespace i5.Toolkit.Core.DeepLinkAPI
             foreach (MethodInfo method in methods)
             {
                 Attribute[] attributes = method.GetCustomAttributes(typeof(DeepLinkAttribute)).ToArray();
+                // check every DeepLinkAttribute on the method
                 foreach (DeepLinkAttribute attribute in attributes)
                 {
                     if (path.Equals(attribute.Path.ToLower()))
                     {
                         ParameterInfo[] parameters = method.GetParameters();
 
+                        // if the method provides a DeepLinkArgs argument, fill it, otherwise do not provide arguments
                         if (parameters.Length == 0)
                         {
                             method.Invoke(instance, null);
@@ -122,10 +161,7 @@ namespace i5.Toolkit.Core.DeepLinkAPI
                             Dictionary<string, string> fragments = UriUtils.GetUriParameters(uri);
                             DeepLinkArgs args = new DeepLinkArgs(fragments, uri);
 
-                            method.Invoke(
-                                instance,
-                                new object[] { args }
-                                );
+                            method.Invoke(instance, new object[] { args });
                         }
                         else
                         {

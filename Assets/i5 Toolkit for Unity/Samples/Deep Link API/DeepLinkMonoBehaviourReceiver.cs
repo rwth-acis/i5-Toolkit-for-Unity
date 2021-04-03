4823b73ced21695a0446be38488b0193bff75af6
Benedikt Hensen <hensen@dbis.rwth-aachen.de>
2021-04-03 19:41:23 +0200

    updated deep linking example

---
+++ Assets/i5 Toolkit for Unity/Samples/Deep Link API/DeepLinkMonoBehaviourReceiver.cs
@@ -0,0 +1,52 @@
+﻿using i5.Toolkit.Core.DeepLinkAPI;
+using i5.Toolkit.Core.ServiceCore;
+using i5.Toolkit.Core.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace i5.Toolkit.Core.Examples.DeepLinkAPI
+{
+    public class DeepLinkMonoBehaviourReceiver : MonoBehaviour
+    {
+        private Renderer rend;
+
+        private void Awake()
+        {
+            rend = GetComponent<Renderer>();
+        }
+
+        private void Start()
+        {
+            ServiceManager.GetService<DeepLinkingService>().AddDeepLinkListener(this);
+        }
+
+        private void OnDestroy()
+        {
+            if (ServiceManager.ServiceExists<DeepLinkingService>())
+            {
+                ServiceManager.GetService<DeepLinkingService>().RemoveDeepLinkListener(this);
+            }
+        }
+
+        [DeepLink("changeColor")]
+        public void ChangeColor(DeepLinkArgs args)
+        {
+            if (args.Parameters.TryGetValue("color", out string color))
+            {
+                if (ColorUtility.TryParseHtmlString(color, out Color parsedColor))
+                {
+                    rend.material.color = parsedColor;
+                }
+                else
+                {
+                    i5Debug.LogWarning("Could not parse color " + color, this);
+                }
+            }
+            else
+            {
+                i5Debug.LogWarning("Deep Link did not contain parameter color", this);
+            }
+        }
+    }
+}

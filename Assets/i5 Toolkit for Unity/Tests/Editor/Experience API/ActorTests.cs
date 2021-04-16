c4a3a92fe9de545edb3c9cca8a6f9818fbffc09e
Benedikt Hensen <hensen@dbis.rwth-aachen.de>
2021-04-16 20:37:29 +0200

    added actor tests

---
+++ Assets/i5 Toolkit for Unity/Tests/Editor/Experience API/ActorTests.cs
@@ -0,0 +1,28 @@
+﻿using i5.Toolkit.Core.ExperienceAPI;
+using NUnit.Framework;
+
+namespace i5.Toolkit.Core.Tests.ExperienceAPI
+{
+    public class ActorTests
+    {
+        private const string mailAddress = "tester@i5toolkit.com";
+
+        [Test]
+        public void Constr_MailWithoutMailto_AddsMailto()
+        {
+            Actor actor = new Actor(mailAddress);
+
+            Assert.AreEqual($"mailto:{mailAddress}", actor.mbox);
+        }
+
+        [Test]
+        public void Constr_MailWithMailto_Unmodified()
+        {
+            string mailto = $"mailto:{mailAddress}";
+
+            Actor actor = new Actor(mailto);
+
+            Assert.AreEqual(mailto, actor.mbox);
+        }
+    }
+}

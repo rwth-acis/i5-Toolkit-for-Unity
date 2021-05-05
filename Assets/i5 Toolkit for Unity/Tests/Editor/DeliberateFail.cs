39c3bb7c616e3519ea3d353b51fbee48905b10fb
Benedikt Hensen <hensen@dbis.rwth-aachen.de>
2021-05-06 00:11:55 +0200

    testing pipeline fail

---
+++ Assets/i5 Toolkit for Unity/Tests/Editor/DeliberateFail.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class DeliberateFail
+    {
+        [Test]
+        public void DeliberateFail_Fails()
+        {
+            Assert.Fail();
+        }
+    }
+}

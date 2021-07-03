56073edd9cd928140f7109b03b6e168481df8c48
Benedikt Hensen <hensen@dbis.rwth-aachen.de>
2021-07-03 20:15:28 +0200

    added wrapper interfaces for transforms

--- Assets/i5 Toolkit for Unity/Runtime/Utilities/UnityWrappers/ITransformable.cs
+++ Assets/i5 Toolkit for Unity/Runtime/Utilities/UnityWrappers/ITransformable.cs
@@ -6,6 +6,20 @@ namespace i5.Toolkit.Core.Utilities.UnityWrappers
 {
     public interface ITransformable
     {
-        
+        Vector3 Position { get; set; }
+
+        Vector3 LocalPosition { get; set; }
+
+        Quaternion Rotation { get; set; }
+
+        Quaternion LocalRotation { get; set; }
+
+        Vector3 EulerAngles { get; set; }
+
+        Vector3 LocalEulerAngles { get; set; }
+
+        Vector3 LocalScale { get; set; }
+
+        Vector3 LossyScale { get; }
     }
 }

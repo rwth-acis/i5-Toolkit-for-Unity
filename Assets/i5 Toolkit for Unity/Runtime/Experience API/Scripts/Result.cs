a4084fdc80d59edae12493405a173c3fcff381d7
Boris Jovanović <bokisjov@gmail.com>
2021-07-28 18:38:55 +0200

    Added timestamp and changed results

--- Assets/i5 Toolkit for Unity/Runtime/Experience API/Scripts/Result.cs
+++ Assets/i5 Toolkit for Unity/Runtime/Experience API/Scripts/Result.cs
@@ -15,31 +15,46 @@ namespace i5.Toolkit.Core.ExperienceAPI
 		///// The score of the Agent in relation to the success or quality of the experience.
 		///// </summary>
 		//public Object score;
+
 		/// <summary>
 		/// Success property of a Result.
 		/// Indicates whether or not the attempt on the Activity was successful.
 		/// </summary>
 		public bool? success;
+
 		/// <summary>
 		/// Completion property of a Result.
 		/// Indicates whether or not the Activity was completed.
 		/// </summary>
 		public bool? completion;
+
 		/// <summary>
 		/// Response property of a Result.
 		/// A response appropriately formatted for the given Activity.
 		/// </summary>
 		public string response;
+
 		/// <summary>
 		/// Duration property of a Result. 
 		/// Period of time over which the Statement occurred.
 		/// Must be ISO 8601 Duration compatible.
 		/// </summary>
 		public TimeSpan? duration;
+
 		/// <summary>
 		/// Extensions property of a Result. Keys MUST be IRIs. 
 		/// </summary
 		public Dictionary<string, string> extensions;
+
+		/// <summary>
+		/// Adds a measurement attempt as a result of an activity.
+		/// Realised as an extenstion of the Result field.
+		/// </summary>
+		/// <param name="measuredValue">The value of the measurement.</param>
+		public void AddMeasurementAtempt(string measuredValue)
+        {
+			extensions.Add("http://mirage-xr.com/measuredValue", measuredValue);
+        }
 
 		public Result()
         {

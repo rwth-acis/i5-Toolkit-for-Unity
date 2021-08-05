5fd03f6c62f0d481fc487fb3a96b4219d5897a91
Benedikt Hensen <hensen@dbis.rwth-aachen.de>
2021-08-05 13:28:33 +0200

    added box volume and function to fit GameObject into box volume

---
+++ Runtime/Experimental/UnityAdapters/BoxColliderAdapter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace i5.Toolkit.Core.Experimental.UnityAdapters
+{
+    public class BoxColliderAdapter : IBoxVolume
+    {
+        public BoxCollider Adaptee { get; private set; }
+
+        public Vector3 Size { get => Adaptee.size; set => Adaptee.size = value; }
+        public Vector3 Center { get => Adaptee.center; set => Adaptee.center = value; }
+        public Quaternion Rotation { get => Adaptee.transform.rotation; set => Adaptee.transform.rotation = value; }
+    }
+}

7d7a3342b456998f1f437998262636385612853a
Benedikt Hensen <hensen@dbis.rwth-aachen.de>
2021-11-16 17:07:22 +0100

    moved and cleaned up cache sample script

---
+++ Samples/Caching/CachingObjImportTestSceneHandler.cs
@@ -0,0 +1,59 @@
+﻿using i5.Toolkit.Core.Caching;
+using i5.Toolkit.Core.ModelImporters;
+using i5.Toolkit.Core.ServiceCore;
+using UnityEngine;
+
+public class ObjImportTestSceneHandler : MonoBehaviour
+{
+    [SerializeField]
+    public string obj_url;
+    [SerializeField]
+    private bool useCache;
+    [SerializeField]
+    private bool persistentSessions;
+    [SerializeField]
+    private GameObject spawner;
+    [SerializeField]
+    private int numberToLoad;
+
+    private Renderer cubeRenderer;
+
+
+    private void Awake()
+    {
+        cubeRenderer = spawner.GetComponent<Renderer>();
+    }
+
+    private void Start()
+    {
+        if (useCache)
+        {
+            FileCache objCache = new FileCache(sessionPersistence: persistentSessions);
+            ServiceManager.RegisterService(objCache);
+        }
+        ObjImporter importer = new ObjImporter();
+        ServiceManager.RegisterService(importer);
+    }
+
+    private async void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F5))
+        {
+            //start signal
+            //Get the Renderer component from the new cube
+            cubeRenderer.material.SetColor("_Color", Color.red);
+
+            for(int i=0; i<numberToLoad; i++)
+            {
+                Vector3 currentPosition = new Vector3(spawner.transform.position.x, spawner.transform.position.y + ((i+1) * 2), spawner.transform.position.z);
+                GameObject obj = await ServiceManager.GetService<ObjImporter>().ImportAsync(obj_url);
+                obj.transform.position = currentPosition;
+                obj.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+
+            }
+
+            //end signal
+            cubeRenderer.material.SetColor("_Color", Color.green);
+        }
+    }
+}

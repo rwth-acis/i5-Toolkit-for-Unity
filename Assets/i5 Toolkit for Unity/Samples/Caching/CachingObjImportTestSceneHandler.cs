d39884ff92f9ba5e21d9e8c226edbd19b9aae64b
Benedikt Hensen <hensen@dbis.rwth-aachen.de>
2021-11-16 17:07:22 +0100

    moved and cleaned up cache sample script

--- Assets/i5 Toolkit for Unity/Samples/Caching/CachingObjImportTestSceneHandler.cs
+++ Assets/i5 Toolkit for Unity/Samples/Caching/CachingObjImportTestSceneHandler.cs
@@ -1,46 +1,46 @@
-﻿using i5.Toolkit.Core.ModelImporters;
+﻿using i5.Toolkit.Core.Caching;
+using i5.Toolkit.Core.ModelImporters;
 using i5.Toolkit.Core.ServiceCore;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjImportTestSceneHandler : MonoBehaviour
 {
-
+    [SerializeField]
     public string obj_url;
-    public bool useCache;
-    public bool persistentSessions;
-    public GameObject spawner;
-    public int numberToLoad;
+    [SerializeField]
+    private bool useCache;
+    [SerializeField]
+    private bool persistentSessions;
+    [SerializeField]
+    private GameObject spawner;
+    [SerializeField]
+    private int numberToLoad;
 
-    // Start is called before the first frame update
-    void Start()
+    private Renderer cubeRenderer;
+
+
+    private void Awake()
+    {
+        cubeRenderer = spawner.GetComponent<Renderer>();
+    }
+
+    private void Start()
     {
         if (useCache)
         {
-            if (persistentSessions)
-            {
-                FileCache objCache = new FileCache(sessionPersistence: true);
-                ServiceManager.RegisterService(objCache);
-            }
-            else
-            {
-                FileCache objCache = new FileCache();
-                ServiceManager.RegisterService(objCache);
-            }
+            FileCache objCache = new FileCache(sessionPersistence: persistentSessions);
+            ServiceManager.RegisterService(objCache);
         }
         ObjImporter importer = new ObjImporter();
         ServiceManager.RegisterService(importer);
     }
 
-    // Update is called once per frame
     private async void Update()
     {
         if (Input.GetKeyDown(KeyCode.F5))
         {
             //start signal
             //Get the Renderer component from the new cube
-            var cubeRenderer = spawner.GetComponent<Renderer>();
             cubeRenderer.material.SetColor("_Color", Color.red);
 
             for(int i=0; i<numberToLoad; i++)

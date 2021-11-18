d5f27b7655baed374688263a8a1aef4d5cfa50bf
Benedikt Hensen <hensen@dbis.rwth-aachen.de>
2021-11-18 17:42:07 +0100

    updated obj importer example

--- Samples/Importers/ObjImporter/ObjImporterBootstrapper.cs
+++ Samples/Importers/ObjImporter/ObjImporterBootstrapper.cs
@@ -1,7 +1,6 @@
-﻿using i5.Toolkit.Core.ModelImporters;
+﻿using i5.Toolkit.Core.Caching;
+using i5.Toolkit.Core.ModelImporters;
 using i5.Toolkit.Core.ServiceCore;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace i5.Toolkit.Core.Examples.ObjImporterExample
@@ -11,6 +10,10 @@ namespace i5.Toolkit.Core.Examples.ObjImporterExample
     /// </summary>
     public class ObjImporterBootstrapper : BaseServiceBootstrapper
     {
+        [SerializeField]
+        private bool useCache = false;
+
+
         /// <summary>
         /// Initializes the obj importer in the service manager
         /// </summary>
@@ -18,11 +21,28 @@ namespace i5.Toolkit.Core.Examples.ObjImporterExample
         {
             ObjImporter importer = new ObjImporter();
             ServiceManager.RegisterService(importer);
+
+            // in case you want to cache downloaded models:
+            // create a file cache service and register at the service manager
+            // after that: set the obj importer's content loader to a cache aware content loader
+            // it will make use of the cache whenever it is possible
+            if (useCache)
+            {
+                // you can change the file cache's settings in the constructor of the FileCacheService
+                FileCacheService fileCache = new FileCacheService();
+                ServiceManager.RegisterService(fileCache);
+                importer.ContentLoader = new CacheAwareContentLoader();
+            }
         }
 
         protected override void UnRegisterServices()
         {
             ServiceManager.RemoveService<ObjImporter>();
+            // also remove the file cache service since we do not need it anymore
+            if (ServiceManager.ServiceExists<FileCacheService>())
+            {
+                ServiceManager.RemoveService<FileCacheService>();
+            }
         }
     }
 }

420627cb43bcde66bd6b0371b960a05e07686ab4
Benedikt Hensen <hensen@dbis.rwth-aachen.de>
2022-02-03 15:51:52 +0100

    added documentation to utility classes

---
+++ Assets/i5 Toolkit for Unity/Runtime/Utilities/ColorExtensions.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace i5.Toolkit.Core.Utilities
+{
+    /// <summary>
+    /// Extension methods for the Color type
+    /// </summary>
+    public static class ColorExtensions
+    {
+        /// <summary>
+        /// Converts the given color to a float array
+        /// </summary>
+        /// <param name="color">The color to convert</param>
+        /// <param name="includeAlpha">If true, the alpha value is included in the array, otherwise not</param>
+        /// <returns>Returns a float array with three or four values containing the rgb and rgba components</returns>
+        public static float[] ToArray(this Color color, bool includeAlpha = true)
+        {
+            if (includeAlpha)
+            {
+                return new float[] { color.r, color.g, color.b, color.a };
+            }
+            else
+            {
+                return new float[] { color.r, color.g, color.b };
+            }
+        }
+    }
+}
--- Assets/i5 Toolkit for Unity/Runtime/Utilities/VectorUtilities.cs
+++ Assets/i5 Toolkit for Unity/Runtime/Utilities/VectorUtilities.cs
@@ -4,23 +4,46 @@ using UnityEngine;
 
 namespace i5.Toolkit.Core.Utilities
 {
+    /// <summary>
+    /// Extensions and utilities for vectors
+    /// </summary>
     public static class VectorUtilities
     {
+        /// <summary>
+        /// Converts the given vector to a float array
+        /// </summary>
+        /// <param name="vector">The vector to convert</param>
+        /// <returns>Returns a float array with the vector's components</returns>
         public static float[] ToArray(this Vector2 vector)
         {
             return new float[] { vector.x, vector.y };
         }
 
+        /// <summary>
+        /// Converts the given vector to a float array
+        /// </summary>
+        /// <param name="vector">The vector to convert</param>
+        /// <returns>Returns a float array with the vector's components</returns>
         public static float[] ToArray(this Vector3 vector)
         {
             return new float[] { vector.x, vector.y, vector.z };
         }
 
+        /// <summary>
+        /// Converts the given vector to a float array
+        /// </summary>
+        /// <param name="vector">The vector to convert</param>
+        /// <returns>Returns a float array with the vector's components</returns>
         public static float[] ToArray(this Vector4 vector)
         {
             return new float[] { vector.x, vector.y, vector.z, vector.w };
         }
 
+        /// <summary>
+        /// Converts a float array to a Vector4
+        /// </summary>
+        /// <param name="array">The array to convert</param>
+        /// <returns>Returns a Vector4 where the components are the first four entries of the array</returns>
         public static Vector4 Vector4FromArray(float[] array)
         {
             Vector4 result = new Vector4();
@@ -31,12 +54,22 @@ namespace i5.Toolkit.Core.Utilities
             return result;
         }
 
+        /// <summary>
+        /// Converts a float array to a Vector3
+        /// </summary>
+        /// <param name="array">The array to convert</param>
+        /// <returns>Returns a Vector3 where the components are the first three entries of the array</returns>
         public static Vector3 Vector3FromArray(float[] array)
         {
             // automatically converts
             return Vector4FromArray(array);
         }
 
+        /// <summary>
+        /// Converts a float array to a Vector2
+        /// </summary>
+        /// <param name="array">The array to convert</param>
+        /// <returns>Returns a Vector2 where the components are the first three entries of the array</returns>
         public static Vector2 Vector2FromArray(float[] array)
         {
             // automatically converts

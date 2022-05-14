19aca92ee63fdad873b50c1b9354f94b0a7f726b
Benedikt Hensen <hensen@dbis.rwth-aachen.de>
2022-05-14 15:55:09 +0200

    added data objects for other RocketChat API methods

---
+++ Runtime/RocketChat Client/DataObjects/Channel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace i5.Toolkit.Core.RocketChatClient
+{
+    [Serializable]
+    public class Channel
+    {
+        public string _id;
+        public string fname;
+        public string description;
+        public string name;
+        public int msgs;
+        public int userCount;
+    }
+}

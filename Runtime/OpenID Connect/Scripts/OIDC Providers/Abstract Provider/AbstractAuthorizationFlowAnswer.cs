c3d3831f7161f9d16808f2c5d9aed76ff7109e89
JulianStaab <julianstaab@web.de>
2022-10-07 16:27:50 +0200

    Created Abstract OIDC Provider classes

---
+++ Runtime/OpenID Connect/Scripts/OIDC Providers/Abstract Provider/AbstractAuthorizationFlowAnswer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace i5.Toolkit.Core.OpenIDConnectClient
+{
+    /// <summary>
+    /// Data description of the answer that is received after the access token was requested in the
+    /// authorization flow
+    /// </summary>
+    [Serializable]
+    public class AbstractAuthorizationFlowAnswer
+    {
+        public abstract string access_token;
+        public abstract string token_type;
+        public abstract string scope;
+    }
+}

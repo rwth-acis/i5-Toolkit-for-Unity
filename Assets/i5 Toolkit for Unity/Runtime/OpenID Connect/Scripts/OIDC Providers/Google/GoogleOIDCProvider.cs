79b24fd75382a9879d9ed251c26aa24e4b331e55
EmilieHK <emiliehastrupkiil@gmail.com>
2022-10-23 17:37:17 +0200

    Set up general Google Oidc provider

---
+++ Assets/i5 Toolkit for Unity/Runtime/OpenID Connect/Scripts/OIDC Providers/Google/GoogleOIDCProvider.cs
@@ -0,0 +1,147 @@
+﻿using i5.Toolkit.Core.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using System.Security.Cryptography;
+
+namespace i5.Toolkit.Core.OpenIDConnectClient
+{
+    /// <summary>
+    /// Implementation of the OpenID Connect Learning Layers Provider
+    /// More information can be found here: https://auth.las2peer.org/auth/
+    /// </summary>
+    public class GoogleOidcProvider : AbstractOidcProvider
+    {
+        /// <summary>
+        /// The generated valid session token
+        /// </summary>
+        private string state;
+
+        /// <summary>
+        /// Creates a new instance of the Learning Layers client
+        /// </summary>
+        public GoogleOidcProvider() : base()
+        {
+            serverName = "https://accounts.google.com/.well-known/openid-configuration";
+        }
+
+        private void GenerateCSRFToken()
+        {
+            // TODO
+            // the state must be generated as in the documentation
+            RandomNumberGenerator generator = RandomNumberGenerator.Create();
+            string token = "";
+            state = token;
+        }
+
+
+        /// <summary>
+        /// Gets the access token based on a previously retrieved authorization code
+        /// </summary>
+        /// <param name="code">The authorization code</param>
+        /// <param name="redirectUri">The redirect URI which was used during the login</param>
+        /// <returns>Returns the access token if it could be retrieved; otherwise it returns an empty string</returns>
+        public override async Task<string> GetAccessTokenFromCodeAsync(string code, string redirectUri)
+        {
+            // Hier hab ich die Reihenfolge der Argumente hoffentlich richtig angepasst und den Flow Typen angepasst, ich denke das könnte so stimmen
+
+            EndpointsData endpoints = await SetEndpoints();
+            if (ClientData == null)
+            {
+                i5Debug.LogError("No client data supplied for the OpenID Connect Client.\n" +
+                    "Initialize this provider with an OpenID Connect Data file.", this);
+                return "";
+            }
+
+            WWWForm form = new WWWForm();
+            form.AddField("code", code);
+            form.AddField("client_id", ClientData.ClientId);
+            form.AddField("client_secret", ClientData.ClientSecret);
+            form.AddField("redirect_uri", redirectUri);
+            form.AddField("grant_type", "authorization_code");
+
+            Dictionary<string, string> headers = new Dictionary<string, string>()
+            {
+                { "Content-Type", "application/x-www-form-urlencoded" }
+            };
+            WebResponse<string> response = await RestConnector.PostAsync(tokenEndpoint, form.data, headers);
+            if (response.Successful)
+            {
+                GoogleAuthorizationFlowAnswer answer =
+                    JsonSerializer.FromJson<GoogleAuthorizationFlowAnswer>(response.Content);
+                if (answer == null)
+                {
+                    i5Debug.LogError("Could not parse access token in code flow answer", this);
+                    return "";
+                }
+                return answer.access_token;
+            }
+            else
+            {
+                Debug.LogError(response.ErrorMessage + ": " + response.Content);
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// Extracts the authorization code from parameters of a Web answer
+        /// </summary>
+        /// <param name="redirectParameters">Parameters of a Web answer as a dictionary</param>
+        /// <returns>The authorization code if it could be found, otherwise an empty string is returned</returns>
+        public override string GetAuthorizationCode(Dictionary<string, string> redirectParameters)
+        {
+            // Hier habe ich den state check eingebaut, ich denke das reicht so
+
+            if (redirectParameters.ContainsKey("state") && redirectParameters["state"].Equals(state))
+            {
+                if (redirectParameters.ContainsKey("code"))
+                {
+                    return redirectParameters["code"];
+                }
+                else
+                {
+                    i5Debug.LogError("Redirect parameters did not contain authorization code", this);
+                    return "";
+                }
+            } 
+            else
+            {
+                i5Debug.LogError("Invalid state parameter", this);
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// Gets information about the logged in user from the provider
+        /// </summary>
+        /// <param name="accessToken">The access token to authenticate the user</param>
+        /// <returns>Returns information about the logged in user if the request was successful, otherwise null</returns>
+        public override async Task<IUserInfo> GetUserInfoAsync(string accessToken)
+        {
+            //TODO
+            // User Info wird nicht nochmal in einem Webrequest requested, sondern steht codiert in dem id_token
+            // Vielleicht ist es aber auch besser, das id_token zu entfernen und stattdessen wie in der abstrakten Klasse einfach über
+            // den UserInfoEndpoint die Info zu requesten (sollte spezifisch dann die GoogleUserInfo als Typ generieren)
+        }
+
+        /// <summary>
+        /// Opens the login page in the system's default Web browser, sets the required endpoints
+        /// </summary>
+        /// <param name="scopes">The OpenID Connect scopes that the user must agree to</param>
+        /// <param name="redirectUri">The URI to which the browser should redirect after the successful login</param>
+        public override async Task<string> OpenLoginPageAsync(string[] scopes, string redirectUri)
+        {
+            // Hier habe ich schon die Reihenfolge der Argumente in der URI angepasst (ich hoffe auch richtig) und die state generation hinzugefügt
+
+            EndpointsData endpoints = await SetEndpoints();
+            GenerateCSRFToken();
+            string responseType = AuthorizationFlow == AuthorizationFlow.AUTHORIZATION_CODE ? "code" : "token";
+            string uriScopes = UriUtils.WordArrayToSpaceEscapedString(scopes);
+            string uri = authorizationEndpoint + $"?response_type={responseType}" + $"&client_id={ClientData.ClientId}" + 
+                    $"&scope={uriScopes}" + $"&redirect_uri={redirectUri}" + $"state={state}";
+            Browser.OpenURL(uri);
+            return uri;
+        }
+    }
+}

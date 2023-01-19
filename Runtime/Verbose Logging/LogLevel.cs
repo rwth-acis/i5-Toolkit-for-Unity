2268794809bffda2bbed87cdea742a2f82b926d0
Benedikt Hensen <hensen@dbis.rwth-aachen.de>
2023-01-19 16:32:10 +0100

    added a module for verbose logging

---
+++ Runtime/Verbose Logging/LogLevel.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum LogLevel
+{
+    CRITICAL = 0,
+    ERROR = 1,
+    WARNING = 2,
+    INFO = 3,
+    DEBUG = 4,
+    TRACE = 5
+}

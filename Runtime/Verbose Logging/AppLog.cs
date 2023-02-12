1b6d1fb641a19cade67118cb01ea06f7d17d1e42
Benedikt Hensen <hensen@dbis.rwth-aachen.de>
2023-02-12 19:24:03 +0100

    fixed spacing if context is not given

--- Runtime/Verbose Logging/AppLog.cs
+++ Runtime/Verbose Logging/AppLog.cs
@@ -1,6 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.VersionControl;
+using i5.Toolkit.Core.Utilities;
 using UnityEngine;
 
 namespace i5.Toolkit.Core.VerboseLogging
@@ -57,6 +55,16 @@ namespace i5.Toolkit.Core.VerboseLogging
 		}
 
         /// <summary>
+		/// Logs a critical error message
+		/// </summary>
+		/// <param name="message">The message to log</param>
+		/// <param name="context">The context of the message</param>
+		public static void LogCritical(string message, object context)
+        {
+            Log(message, LogLevel.CRITICAL, context);
+        }
+
+        /// <summary>
         /// Logs an error message
         /// </summary>
         /// <param name="message">The message to log</param>
@@ -66,13 +74,23 @@ namespace i5.Toolkit.Core.VerboseLogging
 			Log(message, LogLevel.ERROR, context);
 		}
 
-		/// <summary>
-		/// Logs an exception either as an error message or a critical error message
-		/// </summary>
-		/// <param name="e">The exception which should be logged</param>
-		/// <param name="isCritical">If set to true, the message will be logged as a critical error</param>
-		/// <param name="context">The context of the message</param>
-		public static void LogException(System.Exception e, bool isCritical= false, Object context = null)
+        /// <summary>
+        /// Logs an error message
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="context">The context of the message</param>
+        public static void LogError(string message, object context)
+        {
+            Log(message, LogLevel.ERROR, context);
+        }
+
+        /// <summary>
+        /// Logs an exception either as an error message or a critical error message
+        /// </summary>
+        /// <param name="e">The exception which should be logged</param>
+        /// <param name="isCritical">If set to true, the message will be logged as a critical error</param>
+        /// <param name="context">The context of the message</param>
+        public static void LogException(System.Exception e, bool isCritical= false, Object context = null)
 		{
 			LogLevel level = LogLevel.ERROR;
 			if (isCritical)
@@ -81,6 +99,22 @@ namespace i5.Toolkit.Core.VerboseLogging
 			}
 			Log(e.ToString(), level, context);
 		}
+
+        /// <summary>
+        /// Logs an exception either as an error message or a critical error message
+        /// </summary>
+        /// <param name="e">The exception which should be logged</param>
+        /// <param name="isCritical">If set to true, the message will be logged as a critical error</param>
+        /// <param name="context">The context of the message</param>
+        public static void LogException(System.Exception e, bool isCritical, object context)
+        {
+            LogLevel level = LogLevel.ERROR;
+            if (isCritical)
+            {
+                level = LogLevel.CRITICAL;
+            }
+            Log(e.ToString(), level, context);
+        }
 
         /// <summary>
         /// Logs a warning message
@@ -93,6 +127,16 @@ namespace i5.Toolkit.Core.VerboseLogging
 		}
 
         /// <summary>
+        /// Logs a warning message
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="context">The context of the message</param>
+        public static void LogWarning(string message, object context)
+        {
+            Log(message, LogLevel.WARNING, context);
+        }
+
+        /// <summary>
         /// Logs an info message
         /// </summary>
         /// <param name="message">The message to log</param>
@@ -101,6 +145,16 @@ namespace i5.Toolkit.Core.VerboseLogging
 		{
 			Log(message, LogLevel.INFO, context);
 		}
+
+        /// <summary>
+        /// Logs an info message
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="context">The context of the message</param>
+        public static void LogInfo(string message, object context)
+        {
+            Log(message, LogLevel.INFO, context);
+        }
 
         /// <summary>
         /// Logs a message for debugging the code
@@ -113,6 +167,16 @@ namespace i5.Toolkit.Core.VerboseLogging
 		}
 
         /// <summary>
+        /// Logs a message for debugging the code
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="context">The context of the message</param>
+        public static void LogDebug(string message, object context)
+        {
+            Log(message, LogLevel.DEBUG, context);
+        }
+
+        /// <summary>
         /// Logs a message which can be used to trace the code execution path
         /// </summary>
         /// <param name="message">The message to log</param>
@@ -122,60 +186,115 @@ namespace i5.Toolkit.Core.VerboseLogging
 			Log(message, LogLevel.TRACE, context);
 		}
 
-		/// <summary>
-		/// Logs a message with a given lelve
-		/// </summary>
-		/// <param name="message">The message to log</param>
-		/// <param name="level">The importance level of the message</param>
-		/// <param name="context">The context of the message</param>
-		public static void Log(string message, LogLevel level, Object context = null)
+        /// <summary>
+        /// Logs a message which can be used to trace the code execution path
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="context">The context of the message</param>
+        public static void LogTrace(string message, object context)
+        {
+            Log(message, LogLevel.TRACE, context);
+        }
+
+        /// <summary>
+        /// Logs a message with a given level
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="level">The importance level of the message</param>
+        /// <param name="context">The Unity Object context which sends the message</param>
+        public static void Log(string message, LogLevel level, Object context = null)
 		{
 			if (level <= MinimumLogLevel)
 			{
-				string output = $"[{level}] {message}";
-
-#if UNITY_EDITOR
-				if (UseColors)
-				{
-					string color = "black";
-					switch (level)
-					{
-						case LogLevel.CRITICAL:
-							color = $"#{ColorUtility.ToHtmlStringRGB(CriticalColor)}";
-							break;
-						case LogLevel.ERROR:
-							color = $"#{ColorUtility.ToHtmlStringRGB(ErrorColor)}";
-							break;
-						case LogLevel.WARNING:
-							color = $"#{ColorUtility.ToHtmlStringRGB(WarningColor)}";
-                            break;
-						case LogLevel.INFO:
-							color = $"#{ColorUtility.ToHtmlStringRGB(InfoColor)}";
-                            break;
-						case LogLevel.DEBUG:
-							color = $"#{ColorUtility.ToHtmlStringRGB(DebugColor)}";
-                            break;
-						case LogLevel.TRACE:
-							color = $"#{ColorUtility.ToHtmlStringRGB(TraceColor)}";
-							break;
-					}
-                    output = $"<color={color}>[{level}]</color> {message}";
-				}
-#endif
+                string levelOutput = GenerateLevelOutput(level);
+                string contextOutput = GenerateContextOutput(context);
 
 				if (level == LogLevel.CRITICAL || level == LogLevel.ERROR)
 				{
-					Debug.LogError(output, context);
+                    Debug.LogError($"{levelOutput}{contextOutput}{message}", context);
 				}
 				else if (level == LogLevel.WARNING)
 				{
-					Debug.LogWarning(output, context);
-				}
+                    Debug.LogWarning($"{levelOutput}{contextOutput}{message}", context);
+                }
 				else
 				{
-					Debug.Log(output, context);
-				}
+                    Debug.Log($"{levelOutput}{contextOutput}{message}", context);
+                }
 			}
 		}
-	}
+
+        /// <summary>
+        /// Logs a message with a given level
+        /// </summary>
+        /// <param name="message">The message to log</param>
+        /// <param name="level">The importance level of the message</param>
+        /// <param name="context">The object context which sends the message</param>
+		public static void Log(string message, LogLevel level, object context)
+		{
+            if (level <= MinimumLogLevel)
+            {
+				string levelOutput = GenerateLevelOutput(level);
+                string contextOutput = GenerateContextOutput(context);
+
+                if (level == LogLevel.CRITICAL || level == LogLevel.ERROR)
+                {
+                    Debug.LogError($"{levelOutput}{contextOutput}{message}");
+                }
+                else if (level == LogLevel.WARNING)
+                {
+                    Debug.LogWarning($"{levelOutput}{contextOutput}{message}");
+                }
+                else
+                {
+                    Debug.Log($"{levelOutput}{contextOutput}{message}");
+                }
+            }
+        }
+
+		private static string GenerateLevelOutput(LogLevel level)
+		{
+#if UNITY_EDITOR
+            if (UseColors)
+            {
+                string color = "black";
+                switch (level)
+                {
+                    case LogLevel.CRITICAL:
+                        color = $"#{ColorUtility.ToHtmlStringRGB(CriticalColor)}";
+                        break;
+                    case LogLevel.ERROR:
+                        color = $"#{ColorUtility.ToHtmlStringRGB(ErrorColor)}";
+                        break;
+                    case LogLevel.WARNING:
+                        color = $"#{ColorUtility.ToHtmlStringRGB(WarningColor)}";
+                        break;
+                    case LogLevel.INFO:
+                        color = $"#{ColorUtility.ToHtmlStringRGB(InfoColor)}";
+                        break;
+                    case LogLevel.DEBUG:
+                        color = $"#{ColorUtility.ToHtmlStringRGB(DebugColor)}";
+                        break;
+                    case LogLevel.TRACE:
+                        color = $"#{ColorUtility.ToHtmlStringRGB(TraceColor)}";
+                        break;
+                }
+                return $"<color={color}>[{level}]</color> ";
+            }
+#endif
+            return $"[{level}] ";
+        }
+
+        private static string GenerateContextOutput(object context)
+        {
+            if (context == null)
+            {
+                return "";
+            }
+            else
+            {
+                return $"[{context.GetType().ToString()}] ";
+            }
+        }
+    }
 }

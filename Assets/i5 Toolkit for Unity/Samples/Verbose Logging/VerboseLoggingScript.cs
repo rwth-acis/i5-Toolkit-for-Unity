ab897a7f07200bfbc7f3d8db669f85af32255111
Benedikt Hensen <hensen@dbis.rwth-aachen.de>
2023-02-12 19:24:03 +0100

    fixed spacing if context is not given

--- Assets/i5 Toolkit for Unity/Samples/Verbose Logging/VerboseLoggingScript.cs
+++ Assets/i5 Toolkit for Unity/Samples/Verbose Logging/VerboseLoggingScript.cs
@@ -6,11 +6,18 @@ public class VerboseLoggingScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-		AppLog.LogCritical("This is critical!", this);
+        AppLog.LogCritical("This is critical!", this);
         AppLog.LogError("This is an error", this);
         AppLog.LogWarning("This is a warning", this);
         AppLog.LogInfo("This is an info", this);
         AppLog.LogDebug("This is a debug statement", this);
         AppLog.LogTrace("This is a detailed trace", this);
+
+        AppLog.LogCritical("This is critical without context!");
+        AppLog.LogError("This is an error without context");
+        AppLog.LogWarning("This is a warning without context");
+        AppLog.LogInfo("This is an info without context");
+        AppLog.LogDebug("This is a debug statement without context");
+        AppLog.LogTrace("This is a detailed trace without context");
     }
 }

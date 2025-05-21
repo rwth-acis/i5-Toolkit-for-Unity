8c8426d639061d8e4246f522cdbc6a8a957a30cf
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2025-05-21 14:10:29 +0200

    Added recommendation for how to handle keys with version controll

--- Assets/i5 Toolkit for Unity/Runtime/Open AI/APIKey.cs
+++ Assets/i5 Toolkit for Unity/Runtime/Open AI/APIKey.cs
@@ -3,6 +3,8 @@ namespace i5.Toolkit.Core.OpenAI
     public static class APIKey
     {
         // Insert your api key here, but don't commit it to version control!
+        // You can use git update-index --skip-worktree Assets/i5\ Toolkit\ for\ Unity/Runtime/Open\ AI/APIKey.cs to
+        // to ignore changes to this file locally.
         public static string key = "";
     }
 }

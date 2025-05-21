83a5c73b1a69a6635a98f1d82c0a14dd9ddd2ddc
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2025-05-21 15:00:05 +0200

    The api key is now handeld through a text asset

--- Assets/i5 Toolkit for Unity/Runtime/Open AI/APIKey.cs
+++ Assets/i5 Toolkit for Unity/Runtime/Open AI/APIKey.cs
@@ -1,10 +1,36 @@
+using UnityEngine;
+
 namespace i5.Toolkit.Core.OpenAI
 {
+    /// <summary>
+    /// Retrives the API key from the Resources folder. You need to create this folder in Assets and name it EXEACTLY Resources.
+    /// Then create a file called openAI.txt somewhere and import it into Resources. Finally, enter your API key into openAI.txt.
+    /// Never commit this file to source control!
+    /// </summary>
     public static class APIKey
     {
-        // Insert your api key here, but don't commit it to version control!
-        // You can use git update-index --skip-worktree Assets/i5\ Toolkit\ for\ Unity/Runtime/Open\ AI/APIKey.cs to
-        // to ignore changes to this file locally.
-        public static string key = "";
+        private static string _key = "";
+
+        public static string key
+        {
+            get
+            {
+                if (_key == "")
+                {
+                    TextAsset keyAsset = Resources.Load("openAI") as TextAsset;
+                    if (keyAsset != null)
+                    {
+                        _key = keyAsset.text;
+                        _key = _key.Replace("\n", "");
+                    }
+                    else
+                    {
+                        Debug.LogError("Create a Resources folder and import a file called openAI.txt containig your API key!");
+                    }
+                    
+                }
+                return _key;
+            }
+        }
     }
 }
